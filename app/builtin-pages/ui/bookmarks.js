d630fbfe0fed6eea21ca91f633fb987c274ca855
Paul Frazee <pfrazee@gmail.com>
2016-06-16 19:26:02 -0500

    simplify the start page's bookmarks

--- app/builtin-pages/ui/bookmarks.js
+++ app/builtin-pages/ui/bookmarks.js
@@ -16,9 +16,6 @@ var bookmarks = []
 // main rendered element
 var bookmarksTBody
 
-// anything currently being edited?
-var isEditing = false
-
 
 // exported API
 // =
@@ -35,9 +32,6 @@ export function setup () {
     bookmarksTBody = render()
     document.querySelector('.bookmarks').appendChild(bookmarksTBody)
   })
-
-  // register global handlers
-  document.addEventListener('click', onClickAnywhere)
 }
 
 // rendering
@@ -46,20 +40,9 @@ export function setup () {
 function render () {
   return yo`<tbody>
     ${bookmarks.map((b, i) => {
-
-      // render address column
-      var urlCol
-      if (isEditing == i+':url')
-        urlCol = yo`<td><input class="bookmark-url-input" value=${b.url} onclick=${stopEvent} onkeyup=${onKeyupInput.bind(window, i, 'url')}></td>`
-      else
-        urlCol = yo`<td onclick=${onClickColumn.bind(window, i, 'url')}>${b.url}</td>`
-
       // render row
       return yo`<tr>
-        <td onclick=${onClickColumn.bind(window, i, 'title')}>
-          <a href=${b.url} title=${b.title}><span class="icon icon-window"></span> ${b.title}</a>
-        </td>
-        ${urlCol}
+        <td><a href=${b.url} title=${b.title}><span class="icon icon-window"></span> ${b.title}</a></td>
         <td class="actions">
           <span class="icon icon-cancel-squared" onclick=${onClickDelete.bind(window, i)} title="Delete bookmark"></span>
         </td>
@@ -71,25 +54,6 @@ function render () {
 // event handlers
 // =
 
-function onClickColumn (i, key, e) {
-  // do nothing if they clicked on the link
-  if (e.target.tagName == 'A')
-    return
-  e.preventDefault()
-  e.stopPropagation()
-
-  // re-render
-  isEditing = i+':'+key
-  yo.update(bookmarksTBody, render())
-
-  // focus and select all text in the new input
-  var inputEl = bookmarksTBody.querySelector('tr:nth-child('+(i+1)+') .bookmark-'+key+'-input')
-  if (inputEl) {
-    inputEl.focus()
-    inputEl.select()
-  }
-}
-
 function onClickDelete (i) {
   // remove
   var b = bookmarks[i]
@@ -97,41 +61,5 @@ function onClickDelete (i) {
   beaker.bookmarks.remove(b.url)
 
   // render
-  yo.update(bookmarksTBody, render())
-}
-
-function onKeyupInput (i, key, e) {
-  // on enter or escape
-  if (e.keyCode == KEYCODE_ENTER || e.keyCode == KEYCODE_ESC) {
-    // on enter, save kv change
-    if (e.keyCode == KEYCODE_ENTER) {
-      var b = bookmarks[i]
-
-      // update db
-      if (key == 'url')
-        beaker.bookmarks.changeUrl(b.url, e.target.value)
-      if (key == 'title')
-        beaker.bookmarks.changeTitle(b.url, e.target.value)        
-
-      // update in memory
-      b[key] = e.target.value
-    }
-
-    // stop editing
-    isEditing = false
-    yo.update(bookmarksTBody, render())
-  }
-}
-
-function stopEvent (e) {
-  // stop the event, so that onClickAnywhere isnt called
-  e.preventDefault()
-  e.stopPropagation()  
-}
-
-function onClickAnywhere (e) {
-  // stop any editing
-  // (prevented by stopEvent)
-  isEditing = false
   yo.update(bookmarksTBody, render())
 }
