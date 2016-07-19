8410b3d6aade5651f4e038a7f89e1e4c5db5a07b
Paul Frazee <pfrazee@gmail.com>
2016-07-18 21:58:07 -0500

    window now restores previous positioning on open

--- app/background-process/windows.js
+++ app/background-process/windows.js
@@ -1,27 +1,32 @@
 import { app, BrowserWindow, screen } from 'electron'
-import path from 'path'
 import { register as registerShortcut } from 'electron-localshortcut'
+import jetpack from 'fs-jetpack'
+import path from 'path'
 import log from '../log'
+
+// globals
+// =
+var userDataDir
+var stateStoreFile = 'shell-window-state.json'
 
 // exported methods
 // =
 
 export function setup () {
+  // config
+  userDataDir = jetpack.cwd(app.getPath('userData'))
+
   // create first shell window
   createShellWindow()
 }
 
 export function createShellWindow () {
-  // calculate screen size
-  var { width, height } = screen.getPrimaryDisplay().workAreaSize
-  width = Math.max(800, Math.min(1800, width - 50))
-  height = Math.max(600, Math.min(1200, height - 50))
-
   // create window
+  var { x, y, width, height } = ensureVisibleOnSomeDisplay(restoreState())
   var win = new BrowserWindow({ 
     titleBarStyle: 'hidden-inset',
     'standard-window': false,
-    width, height,
+    x, y, width, height,
     webPreferences: {
       webSecurity: false, // disable same-origin policy in the shell-window; the <webview>s of site content will have it re-enabled
     }
@@ -41,6 +46,7 @@ export function createShellWindow () {
   win.on('blur', sendToWebContents('blur'))
   win.on('enter-full-screen', sendToWebContents('enter-full-screen'))
   win.on('leave-full-screen', sendToWebContents('leave-full-screen'))
+  win.on('close', saveState(win))
 
   return win
 }
@@ -51,6 +57,68 @@ export function createShellWindow () {
 function loadURL (win, url) {
   win.loadURL(url)
   log('Opening', url)  
+}
+
+function getCurrentPosition (win) {
+    var position = win.getPosition()
+    var size = win.getSize()
+    return {
+        x: position[0],
+        y: position[1],
+        width: size[0],
+        height: size[1]
+    }
+}
+
+function windowWithinBounds (windowState, bounds) {
+    return windowState.x >= bounds.x &&
+        windowState.y >= bounds.y &&
+        windowState.x + windowState.width <= bounds.x + bounds.width &&
+        windowState.y + windowState.height <= bounds.y + bounds.height
+}
+
+function restoreState () {
+  var restoredState = {}
+  try {
+    restoredState = userDataDir.read(stateStoreFile, 'json')
+  } catch (err) {
+    // For some reason json can't be read (might be corrupted).
+    // No worries, we have defaults.
+  }
+  return Object.assign({}, defaultState(), restoredState)
+}
+
+function defaultState () {
+    var bounds = screen.getPrimaryDisplay().bounds
+    var width = Math.max(800, Math.min(1800, bounds.width - 50))
+    var height = Math.max(600, Math.min(1200, bounds.height - 50))
+    return Object.assign({}, {
+        x: (bounds.width - width) / 2,
+        y: (bounds.height - height) / 2,
+        width,
+        height
+    })
+}
+
+function ensureVisibleOnSomeDisplay (windowState) {
+    var visible = screen.getAllDisplays().some(display => windowWithinBounds(windowState, display.bounds))
+    if (!visible) {
+        // Window is partially or fully not visible now.
+        // Reset it to safe defaults.
+        return defaultState(windowState)
+    }
+    return windowState
+}
+
+function saveState (win) {
+  return e => {
+    // NOTE this is called by .on('close')
+    // if quitting multiple windows at once, the final saved state is unpredictable
+    if (!win.isMinimized() && !win.isMaximized()) {
+      var state = getCurrentPosition(win)
+      userDataDir.write(stateStoreFile, state, { atomic: true })
+    }
+  }
 }
 
 // shortcut event handlers
