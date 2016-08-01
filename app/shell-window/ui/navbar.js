144e07f33e47568b5276e4d8499543a3a8bacefd
Paul Frazee <pfrazee@gmail.com>
2016-08-01 13:23:49 -0500

    update aesthetic of the navbar contextual dat btn

--- app/shell-window/ui/navbar.js
+++ app/shell-window/ui/navbar.js
@@ -126,7 +126,7 @@ function render (id, page) {
     // archive btn
     let info = page.archiveInfo
     // choose label
-    let label = 'View Source'//(info.versionHistory.current) ? `v${info.versionHistory.current}` : ''
+    let label = 'Hostless Site'//(info.versionHistory.current) ? `v${info.versionHistory.current}` : ''
     // render
     archiveBtn = yo`<button class="green" onclick=${onClickViewDat}><span class="icon icon-folder"></span> <small>${label}</small></button>`
   }
