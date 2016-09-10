6eef42a2a6e0c04b1576d92dbbecd942a052967a
Paul Frazee <pfrazee@gmail.com>
2016-09-09 19:11:05 -0500

    can now edit bookmark title and url (close #13)

--- app/builtin-pages/views/favorites.js
+++ app/builtin-pages/views/favorites.js
@@ -40,12 +40,24 @@ export function hide () {
 // =
 
 function render () {
-  const renderRow = (row, i) => yo`<div class="ll-row">
+  const renderRow = (row, i) => row.isEditing ? renderRowEditing(row, i) : renderRowDefault(row, i)
+
+  const renderRowEditing = (row, i) => yo`<div class="ll-row ll-row-is-editing" data-row=${i}>
+    <span class="ll-link">
+      <div class="ll-inputs">
+        <input name="title" value=${row.editTitle} onkeyup=${onKeyUp(i)} />
+        <input name="url" value=${row.editUrl} onkeyup=${onKeyUp(i)} />
+      </div>
+    </div>
+  </div>`
+
+  const renderRowDefault = (row, i) => yo`<div class="ll-row" data-row=${i}>
     <a class="ll-link" href=${row.url} title=${row.title}>
       <img class="favicon" src=${'beaker-favicon:'+row.url} />
       <span class="ll-title">${row.title}</span>
     </a>
     <div class="ll-actions">
+      <span class="icon icon-pencil" onclick=${onClickEdit(i)} title="Edit bookmark"></span>
       <span class="icon icon-cancel" onclick=${onClickDelete(i)} title="Delete bookmark"></span>
     </div>
   </div>`
@@ -73,6 +85,61 @@ function render () {
 
 // event handlers
 // =
+
+function onClickEdit (i) {
+  return e => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    // capture initial value
+    bookmarks[i].editTitle = bookmarks[i].title
+    bookmarks[i].editUrl = bookmarks[i].url
+
+    // enter edit-mode
+    bookmarks[i].isEditing = true
+    render()
+    document.querySelector(`[data-row="${i}"] input`).focus()
+  }
+}
+
+function onKeyUp (i) {
+  return e => {
+    // enter-key
+    if (e.keyCode == 13) {
+      // capture the old url
+      var oldUrl = bookmarks[i].url
+
+      // update values
+      bookmarks[i].title = document.querySelector(`[data-row="${i}"] [name="title"]`).value
+      bookmarks[i].url = document.querySelector(`[data-row="${i}"] [name="url"]`).value
+
+      // exit edit-mode
+      bookmarks[i].isEditing = false
+      render()
+
+      // save in backend
+      beakerBookmarks.changeTitle(oldUrl, bookmarks[i].title)
+      beakerBookmarks.changeUrl(oldUrl, bookmarks[i].url)
+    }
+
+    // escape-key
+    else if (e.keyCode == 27) {
+      // exit edit-mode
+      bookmarks[i].isEditing = false
+      render()
+    }
+
+    // all else
+    else {
+      // update edit values
+      if (e.target.name == 'title')
+        bookmarks[i].editTitle = e.target.value
+      if (e.target.name == 'url')
+        bookmarks[i].editUrl = e.target.value
+    }
+
+  }
+}
 
 function onClickDelete (i) {
   return e => {
