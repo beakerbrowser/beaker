729c4ff8fbb03ad9e7cbceb241d52d73ab66475c
Paul Frazee <pfrazee@gmail.com>
2016-09-19 19:27:09 -0500

    add es-modules support via polyfill

--- app/webview-preload.js
+++ app/webview-preload.js
@@ -1,4 +1,9 @@
 import importWebAPIs from './lib/fg/import-web-apis'
+import babelBrowserBuild from 'browser-es-module-loader/dist/babel-browser-build'
+import BrowserESModuleLoader from 'browser-es-module-loader/dist/browser-es-module-loader'
 
 // setup UI
 importWebAPIs()
+
+// attach globals
+window.BrowserESModuleLoader = BrowserESModuleLoader
