3daafd925ca37b7081fe5016cbdf06bedee37500
Paul Frazee <pfrazee@gmail.com>
2016-09-30 12:18:19 -0500

    add support for CORS, Fetch API, and Service Workers to dat:// sites

--- app/webview-preload.js
+++ app/webview-preload.js
@@ -1,6 +1,19 @@
+import { webFrame } from 'electron'
 import importWebAPIs from './lib/fg/import-web-apis'
 import babelBrowserBuild from 'browser-es-module-loader/dist/babel-browser-build'
 import BrowserESModuleLoader from 'browser-es-module-loader/dist/browser-es-module-loader'
+
+// register protocol behaviors
+webFrame.registerURLSchemeAsPrivileged('dat')
+/* This marks the scheme as:
+ - Secure
+ - Bypassing CSP
+ - Allowing Service Workers
+ - Supporting Fetch API
+ - CORS Enabled
+TODO: we dont want to bypass CSP! that will need some electron fixes
+see https://github.com/electron/electron/issues/7430
+*/
 
 // setup UI
 importWebAPIs()
