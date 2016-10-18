970f22bbb07549346d3291c91cc703bb5d9ac747
Paul Frazee <pfrazee@gmail.com>
2016-10-18 16:44:22 -0500

    disable CSP bypass in dat sites (close #115)

--- app/webview-preload.js
+++ app/webview-preload.js
@@ -5,16 +5,13 @@ import babelBrowserBuild from 'browser-es-module-loader/dist/babel-browser-build
 import BrowserESModuleLoader from 'browser-es-module-loader/dist/browser-es-module-loader'
 
 // register protocol behaviors
-webFrame.registerURLSchemeAsPrivileged('dat')
 /* This marks the scheme as:
  - Secure
- - Bypassing CSP
  - Allowing Service Workers
  - Supporting Fetch API
  - CORS Enabled
-TODO: we dont want to bypass CSP! that will need some electron fixes
-see https://github.com/electron/electron/issues/7430
 */
+webFrame.registerURLSchemeAsPrivileged('dat', { bypassCSP: false })
 
 // setup APIs
 importWebAPIs()
