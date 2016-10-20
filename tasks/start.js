5330a5ecc1a099faae01d4307c9ee6bb93e206b1
Paul Frazee <pfrazee@gmail.com>
2016-10-19 19:00:12 -0500

    add ELECTRON_PATH env var

--- tasks/start.js
+++ tasks/start.js
@@ -1,18 +1,22 @@
-'use strict';
+'use strict'
 
-var childProcess = require('child_process');
-var electron = require('electron');
-var gulp = require('gulp');
+var childProcess = require('child_process')
+var electron = require('electron')
+var gulp = require('gulp')
 
-gulp.task('start', start);
-gulp.task('start-watch', ['watch'], start);
+gulp.task('start', start)
+gulp.task('start-watch', ['watch'], start)
 
 function start () {
+  if (process.env.ELECTRON_PATH) {
+    electron = process.env.ELECTRON_PATH
+  }
+  console.log('Spawning electron', electron)
   childProcess.spawn(electron, ['./app'], {
     stdio: 'inherit'
   })
   .on('close', function () {
     // User closed the app. Kill the host process.
-    process.exit();
-  });
+    process.exit()
+  })
 }
