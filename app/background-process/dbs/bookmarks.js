1708697734779602c9517ad8a2000e72e22e173d
Paul Frazee <pfrazee@gmail.com>
2017-01-20 11:43:30 -0600

    New default bookmarks

--- app/background-process/dbs/bookmarks.js
+++ app/background-process/dbs/bookmarks.js
@@ -87,7 +87,6 @@ migrations = [
       CREATE INDEX bookmarks_url ON bookmarks (url);
       INSERT INTO bookmarks (title, url) VALUES ('Beaker Homepage', 'https://beakerbrowser.com');
       INSERT INTO bookmarks (title, url) VALUES ('Beaker Mailing List', 'https://groups.google.com/forum/#!forum/beaker-browser');
-      INSERT INTO bookmarks (title, url) VALUES ('Dat Protocol', 'https://datproject.org/');
       INSERT INTO bookmarks (title, url) VALUES ('DuckDuckGo (the default search engine)', 'https://duckduckgo.com');
       PRAGMA user_version = 1;
     `, cb)
@@ -99,5 +98,13 @@ migrations = [
       UPDATE bookmarks SET num_visits = 0;
       PRAGMA user_version = 2;
     `, cb)
+  },
+  // version 3
+  function (cb) {
+    db.exec(`
+      INSERT INTO bookmarks (title, url, num_visits) VALUES ('The Hostless Website Portal - Find and share your Dat sites, web 1.0 style', 'dat://1f968afe867f06b0d344c11efc23591c7f8c5fb3b4ac938d6000f330f6ee2a03/', 0);
+      INSERT INTO bookmarks (title, url, num_visits) VALUES ('The Dat Protocol - Decentralized Archive Transport', 'https://www.datprotocol.com', 0);
+      PRAGMA user_version = 3;
+    `, cb)  
   }
 ]
