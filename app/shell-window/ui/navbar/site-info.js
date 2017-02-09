200e34df1cc1f7617acb618fc687ef9f11bbc748
Paul Frazee <pfrazee@gmail.com>
2017-02-08 22:43:01 -0600

    Fix: site info dropdown now renders again

--- app/shell-window/ui/navbar/site-info.js
+++ app/shell-window/ui/navbar/site-info.js
@@ -17,8 +17,89 @@ export class SiteInfoNavbarBtn {
 
   render() {
     // pull details
-    var title = '', url = '', hostname = '', icon = ''
-    var protocolCls = 'insecure', protocolLabel = '', protocolDesc = ''
+    var icon = '', protocolCls = 'insecure', protocolLabel = ''
+    if (this.protocolInfo) {
+      if (['https:'].includes(this.protocolInfo.scheme)) {
+        icon = 'lock'
+        protocolLabel = 'Secure'
+        protocolCls = 'secure'
+      } else if (this.protocolInfo.scheme === 'http:') {
+        icon = 'info-circled'
+      } else if (['dat:', 'fs:'].indexOf(this.protocolInfo.scheme) != -1) {
+        icon = 'share'
+        protocolLabel = 'Secure P2P'
+        protocolCls = 'p2p'
+      }
+    }
+
+    // render btn
+    var iconEl = (icon) ? yo`<span class="icon icon-${icon}"></span>` : ''
+    var titleEl = (protocolLabel) ? yo`<span class="title">${protocolLabel}</span>`: ''
+    return yo`<div class="toolbar-site-info ${protocolCls}">
+      <button onclick=${e => this.toggleDropdown(e)}>${iconEl} ${titleEl}</button>
+    </div>`
+  }
+
+  renderDropdown() {
+    if (!this.isDropdownOpen) {
+      return yo`<div></div>`
+    }
+
+    // pull details
+    var protocolDesc = ''
+    if (this.protocolInfo) {
+      if (['https:'].includes(this.protocolInfo.scheme)) {
+        protocolDesc = 'Your connection to this site is private.'
+      } else if (this.protocolInfo.scheme === 'http:') {
+        protocolDesc = 'Your connection to this site is not private.'
+      } else if (['dat:', 'fs:'].indexOf(this.protocolInfo.scheme) != -1) {
+        protocolDesc = yo`<span>
+          This site was downloaded from a secure peer-to-peer network.
+          <a onclick=${e => this.learnMore()}>Learn More</a>
+        </span>`
+      }
+    }
+
+    // site controls
+    let siteCtrlsEl = ''
+    if (this.protocolInfo) {
+      if (this.protocolInfo.scheme === 'dat:') {
+        siteCtrlsEl = yo`<div><hr /><span>
+          <a onclick=${e => this.viewSiteFiles('files')}>View site files</a>|
+          <a onclick=${e => this.viewSiteFiles('fork')}>Fork site</a>
+        </span></div>`
+      }
+    }
+
+    // site permissions
+    var permsEls = []
+    if (this.sitePerms) {
+      for (var k in this.sitePerms) {
+        permsEls.push(this.renderPerm(k, this.sitePerms[k]))
+      }
+    }
+    if (this.siteInfo && this.siteInfo.requiresRefresh) {
+      permsEls.push(yo`<div>
+        <a><label class="checked" onclick=${this.onClickRefresh.bind(this)}><span class="icon icon-ccw"></span> Refresh to apply changes.</label></a>
+      </div>`)
+    }
+
+    // dropdown
+    return yo`<div class="toolbar-dropdown toolbar-site-info-dropdown">
+      <div class="details">
+        <div class="details-title">
+          <img src="beaker-favicon:${this.getUrl()}" />
+          <strong>${this.getTitle() || this.getHostname() || this.getUrl()}</strong>
+        </div>
+        <div><small>${protocolDesc}</small></div>
+        ${siteCtrlsEl}
+      </div>
+      <div class="perms">${permsEls}</div>
+    </div>`
+  }
+
+  getTitle() {
+    var title = ''
     if (this.siteInfoOverride && this.siteInfoOverride.title) {
       title = this.siteInfoOverride.title
     } else if (this.siteInfo && this.siteInfo.title) {
@@ -26,80 +107,31 @@ export class SiteInfoNavbarBtn {
     } else if (this.protocolInfo && this.protocolInfo.scheme === 'dat:') {
       title = 'Untitled'
     }
-    if (this.protocolInfo) {
-      url = this.protocolInfo.url
-      hostname = this.protocolInfo.hostname
-      if (['https:'].includes(this.protocolInfo.scheme)) {
-        icon = 'lock'
-        protocolLabel = 'Secure'
-        protocolDesc = 'Your connection to this site is private.'
-        protocolCls = 'secure'
-      } else if (this.protocolInfo.scheme === 'http:') {
-        icon = 'info-circled'
-        protocolDesc = 'Your connection to this site is not private.'
-      } else if (['dat:', 'fs:'].indexOf(this.protocolInfo.scheme) != -1) {
-        icon = 'share'
-        protocolLabel = 'Secure P2P'
-        protocolDesc = yo`<span>
-          This site was downloaded from a secure peer-to-peer network.
-          <a onclick=${e => this.learnMore()}>Learn More</a>
-        </span>`
-        protocolCls = 'p2p'
-      }
-    }
+    return title
+  }
 
-    // render the dropdown if open
-    var dropdownEl = ''
-    if (this.isDropdownOpen) {
-      // site controls
-      let siteCtrlsEl = ''
-      if (this.protocolInfo) {
-        if (this.protocolInfo.scheme === 'dat:') {
-          siteCtrlsEl = yo`<div><hr /><span>
-            <a onclick=${e => this.viewSiteFiles('files')}>View site files</a>|
-            <a onclick=${e => this.viewSiteFiles('fork')}>Fork site</a>
-          </span></div>`
-        }
-      }
+  getUrl() {
+    return (this.protocolInfo) ? this.protocolInfo.url : ''
+  }
 
-      // site permissions
-      var permsEls = []
-      if (this.sitePerms) {
-        for (var k in this.sitePerms) {
-          permsEls.push(this.renderPerm(k, this.sitePerms[k]))
-        }
-      }
-      if (this.siteInfo && this.siteInfo.requiresRefresh) {
-        permsEls.push(yo`<div>
-          <a><label class="checked" onclick=${this.onClickRefresh.bind(this)}><span class="icon icon-ccw"></span> Refresh to apply changes.</label></a>
-        </div>`)
-      }
-
-      // dropdown
-      dropdownEl = yo`<div class="toolbar-dropdown toolbar-site-info-dropdown">
-        <div class="details">
-          <div class="details-title">
-            <img src="beaker-favicon:${url}" />
-            <strong>${title || hostname || url}</strong>
-          </div>
-          <div><small>${protocolDesc}</small></div>
-          ${siteCtrlsEl}
-        </div>
-        <div class="perms">${permsEls}</div>
-      </div>`
-    }
-
-    // render
-    var iconEl = (icon) ? yo`<span class="icon icon-${icon}"></span>` : ''
-    var titleEl = (protocolLabel) ? yo`<span class="title">${protocolLabel}</span>`: ''
-    return yo`<div class="toolbar-site-info ${protocolCls}">
-      <button onclick=${e => this.toggleDropdown(e)}>${iconEl} ${titleEl}</button>
-      ${dropdownEl}
-    </div>`
+  getHostname() {
+    return (this.protocolInfo) ? this.protocolInfo.hostname : ''
   }
 
   updateActives() {
+    // FIXME
+    // calling `this.render` for all active site-infos is definitely wrong
+    // there is state captured in `this` that is specific to each instance
+    // ...this entire thing is kind of bad
+    // -prf
     Array.from(document.querySelectorAll('.toolbar-site-info')).forEach(el => yo.update(el, this.render()))
+    Array.from(document.querySelectorAll('.toolbar-site-info-dropdown')).forEach(el => {
+      if (this.isDropdownOpen) {
+        yo.update(el, this.renderDropdown())
+      } else {
+        el.parentNode.removeChild(el)
+      }
+    })
   }
 
   onClickAnywhere(e) {
@@ -126,6 +158,10 @@ export class SiteInfoNavbarBtn {
     }
 
     this.isDropdownOpen = !this.isDropdownOpen
+    if (this.isDropdownOpen) {
+      // create a new dropdown el on the page
+      document.body.appendChild(this.renderDropdown())
+    }
     this.updateActives()
   }
 
