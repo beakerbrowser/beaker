50bed0ad09a6a15bded6edefc784123158456f2e
Paul Frazee <pfrazee@gmail.com>
2017-02-24 09:19:51 -0600

    Remove experimental es6 module support.
    Three reasons:
     1) The module loadtimes were *terrible*.
     2) The debugging experience was only okay.
     3) I think we're going to put buildtools (webpack?) into beaker.

--- app/webview-preload.js
+++ app/webview-preload.js
@@ -2,8 +2,6 @@ import { webFrame } from 'electron'
 import importWebAPIs from './lib/fg/import-web-apis' // TODO remove
 import { setup as setupLocationbar } from './webview-preload/locationbar'
 import { setup as setupNavigatorPermissions } from './webview-preload/navigator-permissions-api'
-import babelBrowserBuild from 'browser-es-module-loader/dist/babel-browser-build'
-import BrowserESModuleLoader from 'browser-es-module-loader/dist/browser-es-module-loader'
 
 import DatArchive from './webview-preload/web-apis/dat-archive'
 
@@ -21,4 +19,3 @@ importWebAPIs()
 setupLocationbar()
 setupNavigatorPermissions()
 window.DatArchive = DatArchive
-window.BrowserESModuleLoader = BrowserESModuleLoader
