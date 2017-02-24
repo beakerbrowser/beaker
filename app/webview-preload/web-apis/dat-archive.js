15c952119bbb0610a1c5ff8cd662c39973943c9a
Paul Frazee <pfrazee@gmail.com>
2017-02-23 21:56:06 -0600

    WIP add dat-archive class definition

---
+++ app/webview-preload/web-apis/dat-archive.js
@@ -0,0 +1,101 @@
+import {ipcRenderer} from 'electron'
+import rpc from 'pauls-electron-rpc'
+import datManifest from 'todo' // TODO
+
+// create the dat rpc api
+const dat = rpc.importAPI('dat', datManifest, { timeout: false, noEval: true })
+
+export default class DatArchive {
+  constructor(opts=null) {
+    let url
+    if (typeof opts === 'string') {
+      // verify existence and validity of url
+      // ... and load into the 'active' (in-memory) cache
+      url = opts
+      dat.loadArchive(url)
+    } else {
+      // create new archive
+      url = dat.createArchive(opts)
+    }
+    // define this.url as a frozen getter
+    Object.defineProperty(this, 'url', {
+      enumerable: true,
+      value: url
+    })
+  }
+
+  static async fork (url, opts=null) {
+    url = (typeof url.url === 'string') ? url.url : url
+    return new DatArchive(dat.forkArchive(url, opts))
+  }
+
+
+  async getInfo(opts=null) {
+    // TODO opts needed?
+    return dat.getInfo(this.url)
+  }
+
+  async setInfo(info) {
+    return dat.setInfo(this.url, info)
+  }
+
+  async stat(path, opts=null) {
+    const url = makeUrl(this.url, path)
+    return dat.stat(url, opts)
+  }
+
+  async readFile(path, opts=null) {
+    const url = makeUrl(this.url, path)
+    return dat.readFile(url, opts)
+  }
+
+  async writeFile(path, data, opts=null) {
+    const url = makeUrl(this.url, path)
+    return dat.writeFile(url, data, opts)
+  }
+
+  async deleteFile(path) {
+    const url = makeUrl(this.url, path)
+    return dat.deleteFile(url)
+  }
+
+  async download(path, opts) {
+    const url = makeUrl(this.url, path)
+    return dat.download(url, opts)
+  }
+
+  async listFiles(path, opts) {
+    const url = makeUrl(this.url, path)
+    return dat.listFiles(url, opts)
+  }
+
+  async createDirectory(path) {
+    const url = makeUrl(this.url, path)
+    return dat.createDirectory(url)
+  }
+
+  async deleteDirectory(path) {
+    const url = makeUrl(this.url, path)
+    return dat.deleteDirectory(url)
+  }
+
+  createFileActivityStream(opts) {
+    // TODO
+  }
+
+  createNetworkActivityStream(opts) {
+    // TODO
+  }
+
+  // static async importFromFilesystem(opts) {
+  //   // TODO
+  // }
+  
+  // static async exportToFilesystem(opts) {
+  //   // TODO
+  // }
+  
+  // static async exportToArchive(opts) {
+  //   // TODO
+  // }
+}
