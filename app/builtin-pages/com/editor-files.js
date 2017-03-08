cd2a9da8a95cd60f75219a26f28512b2c2f1f81a
Tara Vancil <tbvanc@gmail.com>
2017-03-07 21:19:33 -0600

    Pad filetree items based on depth

--- app/builtin-pages/com/editor-files.js
+++ app/builtin-pages/com/editor-files.js
@@ -23,11 +23,11 @@ function redraw (archive) {
   yo.update(document.querySelector('.filetree'), rFileTree(archive))
 }
 
-function rChildren (archive, children) {
+function rChildren (archive, children, depth=0) {
   return Object.keys(children)
     .map(key => children[key])
     .sort(treeSorter)
-    .map(node => rNode(archive, node))
+    .map(node => rNode(archive, node, depth))
 }
 
 function treeSorter (a, b) {
@@ -40,29 +40,38 @@ function treeSorter (a, b) {
   return a.entry.name.localeCompare(b.entry.name)
 }
 
-function rNode (archive, node) {
+function rNode (archive, node, depth) {
   if (node.entry.type === 'directory') {
-    return rDirectory(archive, node)
+    return rDirectory(archive, node, depth)
   }
   if (node.entry.type === 'file') {
-    return rFile(archive, node)
+    return rFile(archive, node, depth)
   }
   return ''
 }
 
-function rDirectory (archive, node) {
+function rDirectory (archive, node, depth) {
   let icon = 'right'
   let children = ''
+  const directoryPadding = 10 + (depth * 10)
+
   const cls = isSelected(archive, node) ? 'selected' : ''
 
   if (node.isExpanded) {
-    children = yo`<div class="subtree">${rChildren(archive, node.children)}</div>`
+    children = yo`
+      <div class="subtree">
+        ${rChildren(archive, node.children, depth + 1)}
+      </div>`
     icon = 'down'
   }
 
   return yo`
     <div>
-      <div class="item folder ${cls}" title=${node.niceName} onclick=${e => onClickDirectory(e, archive, node)}>
+      <div
+        class="item folder ${cls}"
+        title=${node.niceName}
+        onclick=${e => onClickDirectory(e, archive, node)}
+        style=${'padding-left: ' + directoryPadding + 'px'}>
         <i class="fa fa-caret-${icon}"></i>
         ${node.niceName}
       </div>
@@ -71,11 +80,17 @@ function rDirectory (archive, node) {
   `
 }
 
-function rFile (archive, node) {
+function rFile (archive, node, depth) {
   const cls = isSelected(archive, node) ? 'selected' : ''
   const isChanged = archive.dirtyFiles[node.entry.name] ? '*' : ''
+  const padding = depth === 0 ? 20 : 25 + (depth * 5);
+
   return yo`
-    <div class="item file ${cls}" title=${node.niceName} onclick=${e => onClickFile(e, archive, node)}>
+    <div
+      class="item file ${cls}"
+      title=${node.niceName}
+      onclick=${e => onClickFile(e, archive, node)}
+      style=${'padding-left: ' + padding + 'px'}>
       ${node.niceName}${isChanged}
     </div>
   `
