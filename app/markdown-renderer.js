398b3663baecf94101173ba50c0afd4de073f537
Paul Frazee <pfrazee@gmail.com>
2017-03-09 16:55:10 -0600

    Add navigation to rendered markdown

--- app/markdown-renderer.js
+++ app/markdown-renderer.js
@@ -1,6 +1,6 @@
 import Remarkable from 'remarkable'
 
-var md = new Remarkable({
+var md = new Remarkable('full', {
   html:         false,        // Enable HTML tags in source
   xhtmlOut:     false,        // Use '/' to close single tags (<br />)
   breaks:       true,         // Convert '\n' in paragraphs into <br>
@@ -19,35 +19,52 @@ var md = new Remarkable({
   highlight: function (/*str, lang*/) { return ''; }
 });
 
-// hide unformatted el
-var unformattedEl = document.querySelector('body > pre')
-unformattedEl.style.display = 'none'
+// make sure not already handled
+// (for some reason, clicking on a # link inside the page triggers this script again)
+if (!document.querySelector('main')) {
+  // show formatted el
+  var unformattedEl = document.querySelector('body > pre')
+  var formattedEl = document.createElement('main')
+  formattedEl.innerHTML = `<nav></nav><div>${md.render(unformattedEl.textContent)}</div>`
+  document.body.appendChild(formattedEl)
 
-// show formatted el
-var formattedEl = document.createElement('div')
-formattedEl.style.fontFamily = '-apple-system, BlinkMacSystemFont, "Segoe UI", Ubuntu, Cantarell, "Oxygen Sans", "Helvetica Neue", sans-serif'
-formattedEl.style.margin = '1em'
-formattedEl.innerHTML = md.render(unformattedEl.textContent)
-document.body.appendChild(formattedEl)
+  // give ui to switch
+  var a = document.createElement('a')
+  a.className = 'switcher'
+  a.textContent = 'Raw'
+  a.onclick = (e) => {
+    e.preventDefault()
+    if (formattedEl.style.display !== 'none') {
+      formattedEl.style.display = 'none'
+      unformattedEl.style.display = 'block'
+      a.textContent = 'Formatted'
+    } else {
+      formattedEl.style.display = 'flex'
+      unformattedEl.style.display = 'none'
+      a.textContent = 'Raw'
+    }
+  }
+  document.body.appendChild(a)
 
-// give ui to switch
-var a = document.createElement('a')
-a.setAttribute('href', '#')
-a.style.position = 'absolute'
-a.style.top = '5px'
-a.style.right = '5px'
-a.textContent = 'Raw'
-a.onclick = (e) => {
-  e.preventDefault()
-
-  if (unformattedEl.style.display === 'none') {
-    formattedEl.style.display = 'none'
-    unformattedEl.style.display = 'block'
-    a.textContent = 'Formatted'
-  } else {
-    formattedEl.style.display = 'block'
-    unformattedEl.style.display = 'none'
-    a.textContent = 'Raw'
+  // render the nav
+  renderNav()
+  async function renderNav () {
+    var navHTML
+    var self = new DatArchive(location.toString())
+    try {
+      var navMD = await self.readFile('/nav.md')
+      navHTML = md.render(navMD)
+    } catch (e) {
+      var listing = await self.listFiles('/', {depth: 0})
+      navHTML = Object.keys(listing)
+        .filter(name => name.endsWith('md'))
+        .sort()
+        .map(name => `<a href="/${makeSafe(name)}">${makeSafe(name)}</a>`)
+        .join('')
+    }
+    document.querySelector('nav').innerHTML = navHTML
+  }
+  function makeSafe (str) {
+    return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')
   }
 }
-document.body.appendChild(a)
