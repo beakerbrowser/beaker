30e766607642ae6f143a17c8ede032aa8b64ccc3
Tara Vancil <tbvanc@gmail.com>
2017-03-19 16:27:16 -0500

    Make component for editor header

--- app/builtin-pages/com/editor-file-view.js
+++ app/builtin-pages/com/editor-file-view.js
@@ -15,40 +15,6 @@ export default function renderFileView (archive, opts) {
 // renderers
 // =
 
-export function rHeader (archive, path) {
-  if (! (archive && path)) return ''
-  return yo.update(document.querySelector('.editor-header'), yo`
-    <div class="editor-header">
-      <div class="path">
-        ${rFileIcon(path)}
-        <span>${path}</span>
-      </div>
-    </div>`)
-}
-
-function rFileIcon (path) {
-  // lookup the mimetype
-  var mimetype = mime.lookup(path)
-  var cls = 'file-o'
-  console.log(mimetype)
-
-  if (mimetype.startsWith('image/')) {
-    cls = 'file-image-o'
-  } else if (mimetype.startsWith('video/')) {
-    cls = 'file-video-o'
-  } else if (mimetype.startsWith('video/')) {
-    cls = 'file-video-o'
-  } else if (mimetype.startsWith('audio/')) {
-    cls = 'file-audio-o'
-  } else if (mimetype.startsWith('text/html')) {
-    cls = 'file-code-o'
-  } else if (mimetype.startsWith('text/')) {
-    cls = 'file-text-o'
-  }
-
-  return yo`<i class="fa fa-${cls}"></i>`
-}
-
 function rUneditable (archive, path) {
   var el
   // lookup the mimetype
