a5dcd8290a2bb4f61c1bab9d6162747e501b025d
Tara Vancil <tbvanc@gmail.com>
2017-03-19 12:56:56 -0500

    Add error page stylesheet

--- app/background-process/protocols/beaker.js
+++ app/background-process/protocols/beaker.js
@@ -82,6 +82,9 @@ function beakerServer (req, res) {
     return cb(403, 'Forbidden')
   }
 
+  if (requestUrl === 'beaker://assets/error-page.css') {
+    return cb(200, 'OK', 'text/css', path.join(__dirname, 'stylesheets/error-page.css'))
+  }
   // browser ui
   if (requestUrl === 'beaker://shell-window/') {
     return cb(200, 'OK', 'text/html', path.join(__dirname, 'shell-window.html'))
