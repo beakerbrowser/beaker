e5e0cf9c6b446a7071cac5d3532c4133834b8630
Paul Frazee <pfrazee@gmail.com>
2017-03-30 16:29:04 -0500

    Inline the error-page stylesheets

--- app/lib/error-page.js
+++ app/lib/error-page.js
@@ -1,20 +1,106 @@
+var errorPageCSS = `
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+  cursor: pointer;
+}
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Ubuntu, Cantarell, "Oxygen Sans", "Helvetica Neue", sans-serif;
+}
+.btn {
+  display: inline-block;
+  cursor: pointer;
+  color: #5c5c5c;
+  border-radius: 2px;
+  background: #fafafa;
+  border: 1px solid #d9d9d9;
+  font-size: 12px;
+  font-weight: 600;
+  height: 25px;
+  line-height: 25px;
+  padding: 0 8px;
+}
+.btn * {
+  cursor: pointer;
+  line-height: 25px;
+  vertical-align: baseline;
+  display: inline-block;
+}
+.btn:focus {
+  outline-color: #007aff;
+}
+.btn:hover {
+  text-decoration: none;
+  background: #f0f0f0;
+}
+.btn.disabled,
+.btn:disabled {
+  cursor: default;
+  color: #999999;
+  border: 1px solid #ddd;
+  box-shadow: none;
+}
+.btn.disabled .spinner,
+.btn:disabled .spinner {
+  color: #aaa;
+}
+.btn.primary {
+  -webkit-font-smoothing: antialiased;
+  font-weight: 800;
+}
+.btn.primary {
+  background: #007aff;
+  color: #fff;
+  border: none;
+  transition: background .1s ease;
+}
+.btn.primary:hover {
+  background: #0074f2;
+}
+a.btn span {
+  vertical-align: baseline;
+}
+div.error-page-content {
+  max-width: 550px;
+  margin: auto;
+  margin-top: 35vh;
+}
+div.error-page-content p {
+  font-size: 14px;
+  margin: 20px 0;
+}
+div.error-page-content p .error {
+  color: #707070;
+}
+div.error-page-content i {
+  margin-right: 5px;
+}
+div.error-page-content .btn {
+  float: right;
+}
+h1 {
+  margin: 0;
+  color: #333;
+  font-weight: 400;
+  font-size: 22px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #d9d9d9;
+}
+`
+
 export default function (err) {
   return `
     <body>
-    <link rel="stylesheet" href="beaker://assets/font-awesome.css">
-    <link rel="stylesheet" href="beaker://assets/error-page.css">
-
+      <style>${errorPageCSS}</style>
       <div class="error-page-content">
         <h1>This site can’t be reached</h1>
-        <p>
-          Beaker couldn’t establish a connection to this site.
-        </p>
-
-        <p class="error">
-          <i class="fa fa-exclamation-triangle"></i>
-          ${err}
-        </p>
-
+        <p class="error">${err}</p>
         <a class="btn primary" href="javascript:window.location.reload()">Try again</a>
       </div>
     </body>`.replace(/\n/g,'')
