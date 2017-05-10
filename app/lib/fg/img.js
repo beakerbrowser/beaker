704dba0626e2e9146f2fa70bc63a143fadf75a96
Paul Frazee <pfrazee@gmail.com>
2017-05-10 17:06:12 -0500

    Pick optimally-sized favicon

--- app/lib/fg/img.js
+++ app/lib/fg/img.js
@@ -13,6 +13,31 @@ export function urlToData (url, width, height, cb) {
   img.src = url
 }
 
+// like urlToData, but loads all images and takes the one that fits the target dimensions best
+export async function urlsToData (urls, width, height) {
+  var imgs = await Promise.all(urls.map(url => {
+    return new Promise(resolve => {
+      var img = new Image()
+      img.onload = e => resolve(img)
+      img.onerror = () => resolve(false)
+      img.src = url
+    })
+  }))
+  var bestImg = imgs[0]
+  var bestDist = dist(imgs[0].width, imgs[0].height, width, height)
+  for (var i = 1; i < imgs.length; i++) {
+    let imgDist =  dist(imgs[i].width, imgs[i].height, width, height)
+    if (imgDist < bestDist) {
+      bestImg = imgs[i]
+      bestDist = imgDist
+    }
+  }
+  return {
+    url: bestImg.src,
+    dataUrl: imgToData(bestImg, width, height)
+  }
+}
+
 // convert and resize an <img> to a data url
 export function imgToData (img, width, height) {
   var ratio = img.width / img.height
@@ -27,4 +52,8 @@ export function imgToData (img, width, height) {
   var ctx = canvas.getContext('2d')
   ctx.drawImage(img, 0, 0, width, height)
   return canvas.toDataURL('image/png')
+}
+
+function dist (x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
 }
--- app/shell-window/pages.js
+++ app/shell-window/pages.js
@@ -7,7 +7,7 @@ import * as zoom from './pages/zoom'
 import * as navbar from './ui/navbar'
 import * as promptbar from './ui/promptbar'
 import * as statusBar from './ui/statusbar'
-import { urlToData } from '../lib/fg/img'
+import {urlsToData} from '../lib/fg/img'
 import { debounce } from '../lib/functions'
 import errorPage from '../lib/error-page'
 import addAsyncAlternatives from './webview-async'
@@ -101,7 +101,6 @@ export function create (opts) {
     // current page's info
     contentType: null, // what is the content-type of the page?
     favicons: null, // what are the favicons of the page?
-    faviconDominantColor: null, // what's the computed dominant color of favicon?
     bookmark: null, // this page's bookmark object, if it's bookmarked
 
     // current site's info
@@ -759,18 +758,15 @@ function onDidFailLoad (e) {
   }
 }
 
-function onPageFaviconUpdated (e) {
+async function onPageFaviconUpdated (e) {
   if (e.favicons && e.favicons[0]) {
     var page = getByWebview(e.target)
     page.favicons = e.favicons
-    page.faviconDominantColor = null
-    urlToData(e.favicons[0], 64, 64, (err, res) => {
-      if (res) {
-        beakerSitedata.set(page.getURL(), 'favicon', res.url)
-        page.faviconDominantColor = res.dominantColor
-        events.emit('page-favicon-updated', getByWebview(e.target))
-      }
-    })
+    events.emit('page-favicon-updated', getByWebview(e.target))
+
+    // store favicon to db
+    var res = await urlsToData(e.favicons, 64, 64)
+    beakerSitedata.set(page.getURL(), 'favicon', res.dataUrl)
   }
 }
 
