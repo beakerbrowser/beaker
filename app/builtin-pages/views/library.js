687b3752701876556ea7a49e682e113f3cc5a289
Paul Frazee <pfrazee@gmail.com>
2017-06-24 13:36:32 -0500

    Add external-link icon to library titles

--- app/builtin-pages/views/library.js
+++ app/builtin-pages/views/library.js
@@ -338,7 +338,7 @@ function rArchive (archiveInfo) {
     <div class="archive">
       <section class="header">
         <h1 class="title" title=${archiveInfo.title}>
-          <a href="dat://${archiveInfo.key}">${niceName(archiveInfo)}</a>
+          <a href="dat://${archiveInfo.key}">${niceName(archiveInfo)} <i class="fa fa-external-link"></i></a>
           ${archiveInfo.isOwner ? '' : yo`<span class="readonly"><i class="fa fa-eye"></i>Read-only</span>`}
         </h1>
         <div class="actions">
