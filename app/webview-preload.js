c64ff68bd60bf79f54c45c9c20f1fdcd24e6a909
Paul Frazee <pfrazee@gmail.com>
2017-07-10 15:33:08 -0500

    Allow http://localhost to access DatArchive apis

--- app/webview-preload.js
+++ app/webview-preload.js
@@ -21,7 +21,8 @@ webFrame.registerURLSchemeAsPrivileged('dat', { bypassCSP: false })
 
 // setup APIs
 importWebAPIs()
-if (['beaker:','dat:','https:'].includes(window.location.protocol)) {
+if (['beaker:','dat:','https:'].includes(window.location.protocol) || 
+    (window.location.protocol === 'http:' && window.location.hostname === 'localhost')) {
   window.DatArchive = DatArchive
 }
 if (window.location.protocol === 'beaker:') {
