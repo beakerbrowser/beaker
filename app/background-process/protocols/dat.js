270063c6412ec6494866368a704d7f034bb27b8a
Paul Frazee <pfrazee@gmail.com>
2017-07-23 11:33:22 -0500

    Update CSP for dats: dont allow HTTP, but do allow HTTPS

--- app/background-process/protocols/dat.js
+++ app/background-process/protocols/dat.js
@@ -39,9 +39,9 @@ const REQUEST_TIMEOUT_MS = 30e3 // 30 seconds
 
 // content security policies
 const DAT_CSP = `
-default-src * data: blob:;
-script-src * 'unsafe-eval' 'unsafe-inline' data: blob:;
-style-src * 'unsafe-inline' data: blob:;
+default-src dat: https: data: blob:;
+script-src dat: https: 'unsafe-eval' 'unsafe-inline' data: blob:;
+style-src dat: https: 'unsafe-inline' data: blob:;
 object-src 'none';
 `.replace(/\n/g, ' ')
 
