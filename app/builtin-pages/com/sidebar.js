005c00c73bb46875e01e5f6f47de49b57a745315
Tara Vancil <tbvanc@gmail.com>
2017-08-14 10:08:03 -0500

    Variably set icon sizes in sidebar

--- app/builtin-pages/com/sidebar.js
+++ app/builtin-pages/com/sidebar.js
@@ -4,11 +4,11 @@ export default function render (message) {
   return yo`
     <div class="sidebar">
       <a class="sidebar-link" href="beaker://bookmarks">
-        <img src="beaker://assets/icon/star.svg"/>
+        <img class="star" src="beaker://assets/icon/star.svg"/>
         <span class="tooltip">Bookmarks</span>
       </a>
       <a class="sidebar-link" href="beaker://history">
-        <img src="beaker://assets/icon/history.svg"/>
+        <img class="history" src="beaker://assets/icon/history.svg"/>
         <span class="tooltip">History</span>
       </a>
     </div>
