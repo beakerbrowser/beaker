29bc716b455e277b96632d950c79dddda7e17788
Tara Vancil <tbvanc@gmail.com>
2017-08-16 12:53:25 -0500

    Remove unused code from start page

--- app/builtin-pages/views/start.js
+++ app/builtin-pages/views/start.js
@@ -16,24 +16,12 @@ const RELEASE_NOTES_URL = 'https://beakerbrowser.com/releases/0-7-5/?updated=tru
 // globals
 // =
 
-var isManagingBookmarks = false
-var isShelfOpen = false
-var userProfile
-var archivesStatus
 var bookmarks, pinnedBookmarks
-var archivesList
 var settings
 
 setup()
 async function setup () {
   await loadBookmarks()
-  archivesStatus = await beaker.archives.status()
-  userProfile = await beaker.profiles.get(0)
-  try {
-    userProfile.title = (await beaker.archives.get(userProfile.url, {timeout: 500})).title
-  } catch (e) {
-    userProfile.title = 'Your profile'
-  }
   settings = await beakerBrowser.getSettings()
   update()
 
@@ -50,21 +38,6 @@ async function setup () {
       return
     }
   }
-
-  // subscribe to network changes
-  beaker.archives.addEventListener('network-changed', ({details}) => {
-    archivesStatus.peers = details.totalPeerCount
-    yo.update(document.querySelector('a.network'), renderNetworkLink())
-  })
-
-  // load archives list after render (its not pressing)
-  archivesList = new ArchivesList({listenNetwork: true})
-  await archivesList.setup({isSaved: true})
-  archivesList.archives.sort((a, b) => {
-    if (a.url === userProfile.url) return -1
-    if (b.url === userProfile.url) return 1
-    return niceName(a).localeCompare(niceName(b))
-  })
 }
 
 // rendering
@@ -83,73 +56,6 @@ function update () {
       </div>
     </div>
   `)
-}
-
-function renderProfileCard () {
-  return yo`
-    <div class="profile">
-      ${renderNetworkLink()}
-      ${''/* DISABLED <a href=${userProfile.url}>${userProfile.title} <i class="fa fa-user-circle-o"></i></a> */}
-    </div>
-  `
-}
-
-function renderNetworkLink () {
-  return yo`
-    <a class="network" href="beaker://library">
-      <i class="fa fa-share-alt"></i> ${archivesStatus.peers} ${pluralize(archivesStatus.peers, 'peer')}
-    </a>
-  `
-}
-
-function renderShelf () {
-  if (!isShelfOpen) {
-    return yo`
-      <div class="shelf closed" onclick=${toggleShelf}>
-        <i class="fa fa-angle-left"></i>
-      </div>
-    `
-  }
-
-  return yo`
-    <div class="shelf open" onmouseout=${onMouseOutShelf}>
-      <div class="section-header">
-        <h3><a href="beaker://library">Your library</a></h3>
-      </div>
-      <div class="archives-list">
-        ${archivesList.archives.length
-          ? archivesList.archives.map(archiveInfo => {
-            return yo`
-              <a class="archive list-item" href=${`beaker://library/${archiveInfo.key}`}>
-                <span class="title">${niceName(archiveInfo)}</span>
-                <span class="peers">${archiveInfo.peers} ${pluralize(archiveInfo.peers, 'peer')}</span>
-              </a>`
-            })
-          : yo`<p class="no-archives">No archives in your library</p>`}
-      </div>
-
-      <hr />
-
-      <div class="section-header">
-        <h3><a href="beaker://bookmarks">Your bookmarks</a></h3>
-      </div>
-
-      <div class="bookmarks-list">
-        ${bookmarks.length
-          ? bookmarks.map(row => {
-            return yo`
-              <a href=${row.url} class="bookmark list-item">
-                <img class="favicon" src=${'beaker-favicon:' + row.url} />
-                <span href=${row.url} class="bookmark-link" title=${row.title} />
-                  <span class="title">${row.title}</span>
-                </span>
-              </a>`
-          })
-          : yo`<p class="no-bookmarks">No bookmarks</p>`
-        }
-      </div>
-    </div>
-  `
 }
 
 function renderPinnedBookmarks () {
