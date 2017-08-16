1b6d2f288f633865897f61c72b8201db5936364a
Tara Vancil <tbvanc@gmail.com>
2017-08-16 14:00:48 -0500

    Use new builtin page layout for beaker://downloads

--- app/builtin-pages/views/downloads.js
+++ app/builtin-pages/views/downloads.js
@@ -2,6 +2,7 @@ const yo = require('yo-yo')
 const co = require('co')
 const {DownloadsList} = require('builtin-pages-lib')
 import {render as renderDownloadsList} from '../com/downloads-list'
+import renderSidebar from '../com/sidebar'
 
 // globals
 // =
@@ -25,10 +26,19 @@ co(function * () {
 // =
 
 function render () {
-  yo.update(document.querySelector('#el-content'), yo`<div class="pane" id="el-content">
-    <div class="downloads">
-      <h1 class="ll-heading">Downloads</h1>
-      ${renderDownloadsList(downloadsList)}
+  yo.update(document.querySelector('.downloads-wrapper'), yo`
+    <div class="downloads-wrapper builtin-wrapper">
+      ${renderSidebar('downloads')}
+
+      <div>
+        <div class="builtin-sidebar">
+          <h1 class="ll-heading">Downloads</h1>
+        </div>
+
+        <div class="builtin-main">
+          ${renderDownloadsList(downloadsList)}
+        </div>
+      </div>
     </div>
   </div>`)
 }
