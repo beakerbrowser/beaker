4e37ffa95a32f0696e4253dd38109c2d63c91f11
Tara Vancil <tbvanc@gmail.com>
2017-08-19 11:24:00 -0500

    Add default avatar icon for builtin pages

--- app/builtin-pages/com/sidebar.js
+++ app/builtin-pages/com/sidebar.js
@@ -3,10 +3,15 @@ import renderGridIcon from '../icon/grid'
 import renderStarIcon from '../icon/star'
 import renderHistoryIcon from '../icon/history'
 import renderGearIcon from '../icon/gear'
+import renderAvatarIcon from '../icon/avatar'
 
 export default function render (activePage) {
   return yo`
     <div class="sidebar dark">
+      <a class="sidebar-link avatar ${activePage === 'profile' ? 'active' : ''}" href="beaker://profile">
+        ${renderAvatarIcon()}
+        <span class="tooltip avatar">Your profile</span>
+      </a>
       <a class="sidebar-link ${activePage === 'start' ? 'active' : ''}" href="beaker://start">
         ${renderGridIcon()}
         <span class="tooltip start">Start page</span>
---
+++ app/builtin-pages/icon/avatar.js
@@ -0,0 +1,14 @@
+import * as yo from 'yo-yo'
+
+export default function render () {
+  return yo`
+    <svg class="icon avatar" width="120px" height="132px" viewBox="0 0 120 132" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="avatar" fill="#ABB0B6">
+          <path d="M0.728126409,132 C5.22888807,101.403702 30.0521499,78 60,78 C89.9478501,78 114.771112,101.403702 119.271874,132 L0.728126409,132 Z"/>
+          <path d="M59,68 C86.3945312,68.4726562 89,52.7776815 89,34 C89,15.2223185 75.5685425,0 59,0 C42.4314575,0 29,15.2223185 29,34 C29,52.7776815 31.6054688,67.5273438 59,68 Z"/>
+        </g>
+      </g>
+    </svg>
+  `
+}
