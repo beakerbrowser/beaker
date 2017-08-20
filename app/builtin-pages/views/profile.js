70cfe53a3d3c49f1575a0c7d8f4d91cc9ba9c8bc
Tara Vancil <tbvanc@gmail.com>
2017-08-20 11:22:26 -0500

    Wire up basic profile creation/editing

--- app/builtin-pages/views/profile.js
+++ app/builtin-pages/views/profile.js
@@ -5,13 +5,36 @@ import renderSidebar from '../com/sidebar'
 // globals
 // =
 
+var currentProfile
+var isEditingProfile = false
+
 // main
 // =
 
-co(function * () {
+setup()
+async function setup () {
+  currentProfile = await beaker.profiles.getCurrentProfile()
+  console.log(currentProfile)
   // render
   render()
-})
+}
+
+// events
+// =
+
+async function onSaveProfile (e) {
+  e.preventDefault()
+  var name = e.target.name.value || ''
+  var bio = e.target.bio.value || ''
+  await beaker.profiles.setCurrentProfile({name, bio})
+  isEditingProfile = false
+  render()
+}
+
+function onEditProfile () {
+  isEditingProfile = true
+  render()
+}
 
 // rendering
 // =
@@ -20,15 +43,41 @@ function render () {
   yo.update(document.querySelector('.profile-wrapper'), yo`
     <div class="profile-wrapper builtin-wrapper">
       ${renderSidebar('profile')}
-
       <div>
         <div class="builtin-sidebar">
-          <h1>Your profile</h1>
+          ${!isEditingProfile ? yo`<button class="btn" onclick=${onEditProfile}>Edit profile</button>` : ''}
+          ${!currentProfile || isEditingProfile
+            ? renderProfileEditor()
+            : renderProfile()
+          }
         </div>
 
-        <div class="builtin-main">
-        </div>
+        <div class="builtin-main"></div>
       </div>
     </div>
   </div>`)
+}
+
+function renderProfile () {
+  return yo`
+    <div>
+      <p>${currentProfile.name}</p>
+      <p>${currentProfile.bio}</p>
+    </div>
+  `
+}
+
+function renderProfileEditor () {
+  return yo`
+    <div>
+      <form onsubmit=${onSaveProfile}>
+        <label for="name">Name</label>
+        <input type="text" name="name" placeholder="Name" value=${currentProfile.name || ''}/>
+
+        <label for="bio">Bio (optional)</label>
+        <textarea name="bio" placeholder="Enter a short bio" value=${currentProfile.bio || ''}></textarea>
+        <button type="submit" class="btn primary">Save</button>
+      </form>
+    </div>
+  `
 }
