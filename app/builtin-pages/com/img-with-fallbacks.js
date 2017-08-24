8ae807b84927baf8ac2fb807d8a2498dc13c266b
Paul Frazee <pfrazee@gmail.com>
2017-08-24 14:53:32 -0500

    Remove logging

--- app/builtin-pages/com/img-with-fallbacks.js
+++ app/builtin-pages/com/img-with-fallbacks.js
@@ -7,14 +7,12 @@ export default function imgWithFallbacks(baseSrc, exts, {cls} = {}) {
   function render () {
     const nextExt = exts.shift()
     const url = `${baseSrc}.${nextExt}`
-    console.log('trying', nextExt, url)
     return yo`
       <img class=${cls} src=${url} onerror=${onerror} />
     `
   }
 
   function onerror () {
-    console.log('failed', arguments)
     if (exts.length > 0) {
       yo.update(el, render())
     }
