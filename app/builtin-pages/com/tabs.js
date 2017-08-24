1dc76aba7445e23e82ab5076ef8f1fdb022924af
Tara Vancil <tbvanc@gmail.com>
2017-08-24 13:01:38 -0500

    Add dots icon to menu

--- app/builtin-pages/com/tabs.js
+++ app/builtin-pages/com/tabs.js
@@ -1,15 +1,20 @@
 import * as yo from 'yo-yo'
+import renderDotsIcon from '../icon/three-dots.js'
 
 export default function tabs (current, tabs) {
   return yo`
     <div class="nav">
       ${tabs.map(tab => {
-    var cls = 'tab nav-item'
-    if (tab.id === current) {
-      cls += ' active'
-    }
-    return yo`<span class=${cls} onclick=${tab.onclick}>${tab.label}</span>`
-  })}
+        var cls = 'tab nav-item'
+        if (tab.id === current) {
+          cls += ' active'
+        }
+        return yo`<span class=${cls} onclick=${tab.onclick}>${tab.label}</span>`
+      })}
+
+      <span class="nav-item dropdown">
+        ${renderDotsIcon()}
+      </span>
     </div>
   `
 }
