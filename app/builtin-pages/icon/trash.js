2e5bfe11f96c208a13362d540008629ece7d8310
Tara Vancil <tbvanc@gmail.com>
2017-08-29 12:12:31 -0500

    Remove stroke opacity

--- app/builtin-pages/icon/trash.js
+++ app/builtin-pages/icon/trash.js
@@ -4,7 +4,7 @@ import * as svg from '../../lib/fg/svg'
 export default function render () {
   return svg.render(`
     <svg class="icon trash" width="46px" height="47px" viewBox="0 0 46 47" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-opacity="0.4">
+      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="trash" transform="translate(2.000000, 1.000000)" stroke="#000000" stroke-width="4">
           <path d="M37,6 L37,38 C37,41.3137085 34.3137085,44 31,44 L11,44 C7.6862915,44 5,41.3137085 5,38 L5,10 L5,6" id="Rectangle-2"/>
           <path d="M0,1 L42,1" id="Line-3" stroke-linecap="round"/>
