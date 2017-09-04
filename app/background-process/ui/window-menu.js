5c4b0ef4b295a7d637690cfdcdb85194b9234fd3
Paul Frazee <pfrazee@gmail.com>
2017-09-04 11:42:38 -0500

    Ignore a common exception case

--- app/background-process/ui/window-menu.js
+++ app/background-process/ui/window-menu.js
@@ -24,12 +24,16 @@ export function setup () {
 
   // watch for changes to the currently active window
   app.on('browser-window-focus', async (e, win) => {
-    // fetch the current url
-    const url = await win.webContents.executeJavaScript(`pages.getActive().getIntendedURL()`)
+    try {
+      // fetch the current url
+      const url = await win.webContents.executeJavaScript(`pages.getActive().getIntendedURL()`)
 
-    // rebuild as needed
-    if (requiresRebuild(url)) {
-      setApplicationMenu({url})
+      // rebuild as needed
+      if (requiresRebuild(url)) {
+        setApplicationMenu({url})
+      }
+    } catch (e) {
+      // `pages` not set yet
     }
   })
 }
