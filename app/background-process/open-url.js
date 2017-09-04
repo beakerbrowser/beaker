09c3d8f4adbc19b041af3282ed8e972616174014
Paul Frazee <pfrazee@gmail.com>
2017-09-04 15:04:35 -0500

    Fix: open-url event no longer causes exceptions if first window is ever closed

--- app/background-process/open-url.js
+++ app/background-process/open-url.js
@@ -1,21 +1,32 @@
 // handle OSX open-url event
-import { ipcMain } from 'electron'
+import {ipcMain} from 'electron'
+import * as windows from './ui/windows'
 var queue = []
-var commandReceiver
+var isLoaded = false
 
 export function setup () {
-  ipcMain.once('shell-window-ready', function (e) {
-    commandReceiver = e.sender
-    queue.forEach(url => commandReceiver.send('command', 'file:new-tab', url))
+  ipcMain.on('shell-window-ready', function (e) {
+    queue.forEach(url => e.sender.send('command', 'file:new-tab', url))
     queue.length = 0
+    isLoaded = true
   })
 }
 
 export function open (url) {
-  if (commandReceiver) {
-    commandReceiver.send('command', 'file:new-tab', url)
+  var win = windows.getActiveWindow()
+  if (isLoaded && win) {
+    // send command now
+    win.webContents.send('command', 'file:new-tab', url)
   } else {
+    // queue for later
     queue.push(url)
+
+    // no longer loaded?
+    if (isLoaded) {
+      isLoaded = false
+      // spawn a new window
+      windows.createShellWindow()
+    }
   }
-  return commandReceiver
+  return win && win.webContents
 }
