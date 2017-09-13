2e5f308b1e58c230f28cbdbf9cedf5996885baae
Paul Frazee <pfrazee@gmail.com>
2017-09-13 16:24:02 -0500

    Implement rename context menu in files-list

--- app/builtin-pages/com/files-list.js
+++ app/builtin-pages/com/files-list.js
@@ -14,7 +14,9 @@ import renderFilesListSidebar from './files-list-sidebar'
 // opts:
 //  - hideDate: show the date on the files (bool)
 //  - baseUrl: override the base URL of file links (string)
+//  - expandedNodes: paths of nodes that should be expanded (Set of strings)
 export default function render (root, opts = {}) {
+  opts.expandedNodes = opts.expandedNodes || new Set()
   return rFilesList(root, null, opts)
 }
 
@@ -68,7 +70,8 @@ function rDirectory (root, node, selectedNode, depth, opts) {
   const directoryPadding = 20 + (depth * 20)
   const caretPosition = directoryPadding - 15
 
-  if (node.isExpanded) {
+  const isExpanded = opts.expandedNodes.has(node._path)
+  if (isExpanded) {
     children = yo`
       <div class="subtree">
         ${rChildren(root, node.children, selectedNode, depth + 1, opts)}
@@ -88,12 +91,14 @@ function rDirectory (root, node, selectedNode, depth, opts) {
           class="caret"
           ondblclick=${e => e.stopPropagation()}
           onclick=${e => onClickDirectoryCaret(e, root, node, selectedNode, opts)}
-          style="left: ${caretPosition}px; ${node.isExpanded ? 'transform: rotate(90deg);' : ''}"
+          style="left: ${caretPosition}px; ${isExpanded ? 'transform: rotate(90deg);' : ''}"
         >▶︎</div>
 
         <img class="icon folder" src="beaker://assets/icon/folder-color.png"/>
 
-        <div class="name">${node.name}</div>
+        ${node.isRenaming
+          ? yo`<div class="name"><input value=${node.renameValue} onkeyup=${e => onKeyupRename(e, root, node, opts)} /></div>`
+          : yo`<div class="name">${node.name}</div>`}
       </div>
       ${children}
     </div>
@@ -115,9 +120,45 @@ function rFile (root, node, selectedNode, depth, opts) {
 
       <img class="icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAUJJREFUeNqkk8FKw0AQhmcmyb0HTR7A4hPUS1B8haIetPh4Ei9tUqQn6aUPIra9hRYUTKiCzbiTJpsssdXSgWFnJjsf/85ukJnhaTR6+16vW5xlwFA3yRAIERBxcdXtuvoLcxUMooj/sjiOuR+GQR0gTnXa63SmfA7T2TxfJRd7CAJwXRfOff+uH0WPdY12KTRPbEukKhdoBUYiWCyX4HkeXPj+bTgcOqp8owElgshWjpqeZZv6absNz+MxyIzuez0BXxsKSgm2TbmC0ogYPtIEzjqd3DWY2TxC1WAZALHP1ReslEPR5B4f6bgBkBlsM2b+td5QsK8ZAMuiAwF0IECuEYB3bMctgGLyjmMZE272o7mWACqk0z+PUN+XA9IkmajneZlt3u9OBfJnpmk60SW5X/V4TlTc2mN276rvRYIfAQYABXymGHKpbU8AAAAASUVORK5CYII="/>
 
-      <div class="name">${node.name}</div>
+      ${node.isRenaming
+        ? yo`<div class="name"><input value=${node.renameValue} onkeyup=${e => onKeyupRename(e, root, node, opts)} /></div>`
+        : yo`<div class="name">${node.name}</div>`}
     </div>
   `
+}
+
+// helpers
+// =
+
+async function selectNode (root, newNode, selectedNode, opts) {
+  // reset old node
+  if (selectedNode) {
+    selectedNode.isRenaming = false
+  }
+
+  // update state
+  selectedNode = newNode
+
+  // render
+  redraw(root, selectedNode, opts)
+
+  // read data if needed, and redraw
+  if (selectedNode && selectedNode.type === 'file') {
+    await selectedNode.readData()
+    redraw(root, selectedNode, opts)
+  }
+}
+
+async function refreshAllNodes (node, opts) {
+  await node.readData()
+  if (node.hasChildren) {
+    const children = node.children
+    for (var k in children) {
+      if (opts.expandedNodes.has(children[k]._path)) {
+        await refreshAllNodes(children[k], opts)
+      }
+    }
+  }
 }
 
 // event handlers
@@ -128,18 +169,7 @@ async function onClickNode (e, root, node, selectedNode, depth, opts) {
     e.preventDefault()
     e.stopPropagation()
   }
-
-  // update state
-  selectedNode = node
-
-  // render
-  redraw(root, selectedNode, opts)
-
-  // read data if needed, and redraw
-  if (node && node.type === 'file') {
-    await node.readData()
-    redraw(root, selectedNode, opts)
-  }
+  selectNode(root, node, selectedNode, opts)
 }
 
 async function onContextMenu (e, root, node, selectedNode, depth, opts) {
@@ -153,32 +183,32 @@ async function onContextMenu (e, root, node, selectedNode, depth, opts) {
 
   // now run the menu
   var menu
-  const visible = (node || root).isEditable
+  const enabled = (node || root).isEditable
   if (node && node.type === 'file') {
     menu = [
       {label: 'Open URL', id: 'open'},
       {label: 'Copy URL', id: 'copy-url'},
-      {type: 'separator', visible},
-      {label: 'Rename', id: 'rename-file', visible},
-      {label: 'Delete file', id: 'delete-file', visible}
+      {type: 'separator'},
+      {label: 'Rename', id: 'rename', enabled},
+      {label: 'Delete file', id: 'delete', enabled}
     ]
   } else if (node && node.type === 'folder') {
     menu = [
       {label: 'Open URL', id: 'open'},
       {label: 'Copy URL', id: 'copy-url'},
-      {type: 'separator', visible},
-      {label: `New folder in "${node.name}"`, id: 'new-folder', visible},
-      {label: `Import files to "${node.name}"`, id: 'import', visible},
-      {label: 'Rename', id: 'rename-folder', visible},
-      {label: 'Delete folder', id: 'delete-folder', visible}
+      {type: 'separator'},
+      {label: `New folder in "${node.name}"`, id: 'new-folder', enabled},
+      {label: `Import files to "${node.name}"`, id: 'import', enabled},
+      {label: 'Rename', id: 'rename', enabled},
+      {label: 'Delete folder', id: 'delete', enabled}
     ]
   } else {
     menu = [
       {label: 'Open URL', id: 'open'},
       {label: 'Copy URL', id: 'copy-url'},
-      {type: 'separator', visible},
-      {label: 'New folder', id: 'new-folder', visible},
-      {label: 'Import files', id: 'import', visible}
+      {type: 'separator'},
+      {label: 'New folder', id: 'new-folder', enabled},
+      {label: 'Import files', id: 'import', enabled}
     ]
   }
   menu.push({type: 'separator'})
@@ -195,11 +225,23 @@ async function onContextMenu (e, root, node, selectedNode, depth, opts) {
   const action = await beakerBrowser.showContextMenu(menu)
 
   // now run the action
-  console.log(action)
   node = node || root
   switch (action) {
     case 'open': return window.open(node.url)
     case 'copy-url': return writeToClipboard(node.url)
+    case 'rename':
+    {
+      node.isRenaming = true
+      node.renameValue = node.name
+      if (selectedNode !== node) selectNode(root, node, selectedNode, opts)
+      else redraw(root, selectedNode, opts)
+      let input = document.querySelector('.files-list-view input')
+      if (input) {
+        input.focus()
+        input.select()
+      }
+      return
+    }
     case null: return
     default:
       if (action && action.startsWith('new')) {
@@ -221,12 +263,32 @@ function onDblClickNode (e, node) {
   }
 }
 
-async function onClickDirectoryCaret (e, root, node, selectedNode, opts = {}) {
+async function onClickDirectoryCaret (e, root, node, selectedNode, opts) {
   e.preventDefault()
   e.stopPropagation()
-  node.isExpanded = !node.isExpanded
-  if (node.isExpanded) {
+  if (opts.expandedNodes.has(node._path)) {
+    opts.expandedNodes.delete(node._path)
+  } else {
+    opts.expandedNodes.add(node._path)
     await node.readData()
   }
   redraw(root, selectedNode, opts)
+}
+
+async function onKeyupRename (e, root, node, opts) {
+  node.renameValue = e.target.value
+
+  if (e.code === 'Escape') {
+    node.isRenaming = false
+    redraw(root, node, opts)
+  }
+  if (e.code === 'Enter') {
+    node.isRenaming = false
+    // do rename
+    await node.rename(node.renameValue)
+    // reload the tree
+    await refreshAllNodes(root, opts)
+    // redraw with no selected node 
+    redraw(root, null, opts)    
+  }
 }
