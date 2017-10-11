a70657eff4de55b0da6591ae359cd19571299468
Tara Vancil <tbvanc@gmail.com>
2017-10-11 15:19:19 -0500

    Add a tool for controlling seeding length from beaker://network

--- app/builtin-pages/views/network.js
+++ app/builtin-pages/views/network.js
@@ -1,18 +1,35 @@
 /* globals beaker */
 
 import yo from 'yo-yo'
+import moment from 'moment'
 import prettyBytes from 'pretty-bytes'
 import renderSidebar from '../com/sidebar'
 import * as toast from '../com/toast'
 import renderTrashIcon from '../icon/trash'
+import renderGearIcon from '../icon/gear-small'
 import {pluralize} from '../../lib/strings'
+import {findParent} from '../../lib/fg/event-handlers'
 
 // globals
 // =
+const NOT = 0
+const ONEDAY = 1
+const ONEWEEK = 2
+const ONEMONTH = 3
+const FOREVER = 4
+const TIMELENS = [
+  () => yo`<span>Never</span>`,
+  () => yo`<span>1 day</span>`,
+  () => yo`<span>1 week</span>`,
+  () => yo`<span>1 month</span>`,
+  () => yo`<span>Forever</span>`
+]
+let sliderState
 let archives = []
 let totalBytesHosting = 0
 let totalArchivesHosting = 0
 let currentFilter = 'seeding'
+let currentlyConfiguringKey
 
 // main
 // =
@@ -40,6 +57,13 @@ async function fetchArchives () {
 // events
 // =
 
+async function onClickSettings (archive) {
+  currentlyConfiguringKey = archive.key
+  render()
+  document.body.addEventListener('keyup', onKeyUp)
+  document.body.addEventListener('click', onClick)
+}
+
 async function onNetworkChanged ({details}) {
   var archive = archives.find(a => details.url === a.url)
   if (archive) archive.peers = details.peerCount
@@ -48,8 +72,32 @@ async function onNetworkChanged ({details}) {
 
 async function onUpdateFilter (e) {
   currentFilter = e.target.dataset.filter
-  console.log(currentFilter)
+  currentlyConfiguringKey = null
   await fetchArchives()
+  render()
+}
+
+async function onChangeSeedingConfiguration (e) {
+  sliderState = e.target.value
+  const isNetworked = sliderState == NOT ? false : true
+  let expiresAt = 0
+
+  if (sliderState == ONEDAY) expiresAt = +(moment().add(1, 'day'))
+  if (sliderState == ONEWEEK) expiresAt = +(moment().add(1, 'week'))
+  if (sliderState == ONEMONTH) expiresAt = +(moment().add(1, 'month'))
+
+  const tmpArchive = new DatArchive(currentlyConfiguringKey)
+  const tmpArchiveInfo = await tmpArchive.getInfo()
+  if (tmpArchiveInfo.isOwner) {
+    await tmpArchive.configure({networked: isNetworked})
+  } else {
+    if (isNetworked) {
+      await beaker.archives.add(currentlyConfiguringKey, {expiresAt, networked: true})
+    } else {
+      await beaker.archives.remove(currentlyConfiguringKey)
+    }
+  }
+
   render()
 }
 
@@ -76,8 +124,8 @@ async function onToggleHosting (archive) {
     }
   }
 
-  // unsave if not owner and update the peer count
   else {
+    // unsave if not owner and update the peer count
     if (archive.userSettings.isSaved) {
       await beaker.archives.remove(archive.key)
       archive.userSettings.isSaved = false
@@ -99,6 +147,29 @@ async function onUnsaveArchive (archive) {
   archive.userSettings.isSaved = false
   totalArchivesHosting -= 1
   totalBytesHosting -= archive.size
+  render()
+}
+
+function onClick (e) {
+  if (!findParent(e.target, 'gear-btn')) {
+    console.log('destroy')
+    destroySeedingMenu()
+  }
+}
+
+function onKeyUp (e) {
+  e.preventDefault()
+  e.stopPropagation()
+
+  if (e.keyCode === 27) {
+    destroySeedingMenu()
+  }
+}
+
+function destroySeedingMenu () {
+  currentlyConfiguringKey = null
+  document.body.removeEventListener('keyup', onKeyUp)
+  document.body.removeEventListener('click', onClick)
   render()
 }
 
@@ -155,6 +226,67 @@ function renderHeader () {
   }
 }
 
+function renderSeedingMenu (archive) {
+  if (archive.key !== currentlyConfiguringKey) return ''
+
+  // calculate the current state
+  const isNetworked = archive.userSettings.networked
+  const expiresAt = archive.userSettings.expiresAt
+  const now = Date.now()
+  const timeRemaining = (isNetworked && expiresAt && expiresAt > now) ? moment.duration(expiresAt - now) : null
+
+  var currentSetting
+  if (!isNetworked) currentSetting = NOT
+  else if (!expiresAt) currentSetting = FOREVER
+  else if (timeRemaining.asMonths() > 0.5) currentSetting = ONEMONTH
+  else if (timeRemaining.asWeeks() > 0.5) currentSetting = ONEWEEK
+  else currentSetting = ONEDAY
+
+  // configure rendering params
+  sliderState = typeof sliderState === 'undefined' ? currentSetting : sliderState
+
+  const seedingStatusClass =
+    (sliderState == NOT ?
+      'red' :
+      (sliderState == FOREVER ?
+        'green' :
+        'yellow'))
+  const seedingStatusLabel = timeRemaining && typeof sliderState === 'undefined'
+    ? yo`<span>Rehosting (${timeRemaining.humanize()} remaining)</span>`
+    : TIMELENS[sliderState]()
+  return yo`
+    <div class="seeding-menu">
+      <div>
+        <label for="rehost-period">Seed these files</label>
+        <input
+          name="rehost-period"
+          type="range"
+          min="0"
+          max="4"
+          step="1"
+          list="steplist"
+          value=${sliderState}
+          onchange=${e => onChangeSeedingConfiguration(e)}
+          oninput=${e => onChangeSeedingConfiguration(e)} />
+        <datalist id="steplist">
+          <option>0</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+        </datalist>
+      </div>
+
+      <div class="labels">
+        <div class="policy">
+          <i class="circle ${seedingStatusClass}"></i>
+          ${seedingStatusLabel}
+        </div>
+      </div>
+    </div>
+  `
+}
+
 function renderArchives () {
   return yo`
     <ul class="archives">${archives.map(renderArchive)}</ul>
@@ -162,6 +294,17 @@ function renderArchives () {
 }
 
 function renderArchive (archive) {
+  // calculate how much longer the archive will be re-hosted
+  const expiresAt = archive.userSettings.expiresAt
+  const now = Date.now()
+  const timeRemaining = (expiresAt && expiresAt > now) ? moment.duration(expiresAt - now) : null
+
+  let expiresAtStr
+  if (!expiresAt) expiresAtStr = ''
+  else if (timeRemaining.asMonths() > 0.5) expiresAtStr = '(1 month remaining)'
+  else if (timeRemaining.asWeeks() > 0.5) expiresAtStr = '(1 week remaining)'
+  else expiresAtStr = '(1 day remaining)'
+
   return yo`
     <li class="archive">
       <div>
@@ -170,6 +313,9 @@ function renderArchive (archive) {
         <span class="info">
           <a href=${archive.url} class="title">
             ${archive.title || yo`<em>Untitled</em>`}
+          </a>
+
+          <span class="status">
             <span class="circle ${archive.userSettings.networked ? 'green' : 'red'}"></span>
           </a>
 
@@ -177,6 +323,12 @@ function renderArchive (archive) {
             ${archive.peers} ${pluralize(archive.peers, 'peer')}
             <span class="bullet">•</span>
             ${prettyBytes(archive.size)}
+            <span class="bullet">•</span>
+            ${expiresAtStr}
+            <span class="gear-btn" onclick=${e => onClickSettings(archive)}>
+              ${renderGearIcon()}
+              ${renderSeedingMenu(archive)}
+            </span>
           </div>
         </span>
       </div>
