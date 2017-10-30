ca64eb3e269a34b569768d91fe56187386575979
Paul Frazee <pfrazee@gmail.com>
2017-10-30 16:03:00 -0500

    Fixes for appveyor

--- tasks/postbuild.js
+++ tasks/postbuild.js
@@ -3,13 +3,20 @@ const jetpack = require('fs-jetpack')
 const run = require('./util-run')
 
 gulp.task('postbuild', async () => {
-  // OSX - for some reason, electron-builder is spitting out 'Beaker Browser-{version}{ext}'
+  // for some reason, electron-builder is spitting out 'Beaker Browser-{version}{ext}'
   // but the auto updater expects 'beaker-browser-{version}{ext}'
-  // so auto-rename any files like that
+  // couldnt figure out how to reconfig the builder, so just rename the output assets
 
   const cwd = jetpack.cwd('dist')
   const names = await cwd.listAsync()
   await Promise.all(names.map(name => {
+    // windows assets:
+    if (name.startsWith('Beaker Browser Setup') && name.endsWith('.exe')) {
+      const newName = 'beaker-browser-setup-' + name.slice('Beaker Browser Setup '.length)
+      return cwd.move(name, newName)
+    }
+
+    // osx assets:
     if (name.startsWith('Beaker Browser') && (name.endsWith('.dmg') || name.endsWith('-mac.zip'))) {
       const newName = 'beaker-browser' + name.slice('Beaker Browser'.length)
       return cwd.move(name, newName)
