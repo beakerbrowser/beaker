2a680531c2ebeac13b652de823b9c9a97f888972
Tara Vancil <tbvanc@gmail.com>
2017-11-02 12:01:36 -0500

    Wire up workspace creation popup

--- app/builtin-pages/com/create-workspace-popup.js
+++ app/builtin-pages/com/create-workspace-popup.js
@@ -1,11 +1,36 @@
 import yo from 'yo-yo'
 import closeIcon from '../icon/close'
+import * as toast from './toast'
 
 // globals
 // =
+let directory = ''
+let targetURL = ''
 
 var resolve
 var reject
+
+// events
+// =
+
+function onChangeWorkspaceDirectory (e) {
+  directory = e.target.files[0].path
+  document.querySelector('#create-workspace-popup .error').innerText = ''
+  document.querySelector('[data-path]').dataset.path = directory
+}
+
+async function onSelectDat (e) {
+  var archive = await DatArchive.selectArchive({
+    title: 'Choose a target archive',
+    buttonLabel: 'Select',
+    filters: {isOwner: true}
+  })
+  if (archive) {
+    targetURL = archive.url
+    document.querySelector('#create-workspace-popup .error').innerText = ''
+    document.querySelector('[data-url]').dataset.url = archive.url
+  }
+}
 
 // exported api
 // =
@@ -24,22 +49,36 @@ export function render () {
 
         <div class="body">
           <div>
-            <div class="namespace-input-container">
-              <label for="name">Name</label>
+            <label for="name">Local URL</label>
+            <p>
+              The shortcut for previewing your workspace
+            </p>
+            <div class="name-input-container">
               <span class="protocol">workspaces://</span>
-              <input id="name" name="name" type="text" required />
+              <input required type="text" name="name"/>
             </div>
 
-            <label for="url">Target</label>
-            <input name="url" value="dat://63f3fde2e1afbe3fbc3804fc53a9a764fe297f9779e686fd779fd7f7324cb077"/>
+            <label for="url">Target URL</label>
+            <p>
+              The Dat archive that your changes will be published at
+            </p>
+            <button type="button" class="btn url" onclick=${onSelectDat} data-url=${targetURL}>
+              Browse your archives
+            </button>
 
-            <label for="path">Local folder</label>
-            <input name="path" type="file" webkitdirectory required />
+            <label>Directory</label>
+            <p>The directory on your computer that contains your workspace's files</p>
+            <label for="path" class="btn" data-path=${directory}>
+              Select directory
+            </label>
+            <input id="path" name="path" type="file" webkitdirectory onchange=${onChangeWorkspaceDirectory}/>
+
+            <p class="error"></p>
           </div>
 
           <div class="actions">
             <button type="button" class="btn" onclick=${destroy}>Cancel</button>
-            <button type="submit" class="btn primary">Save</button>
+            <button type="submit" class="btn success">Create workspace</button>
           </div>
         </div>
       </form>
@@ -69,6 +108,8 @@ export function destroy () {
   var popup = document.getElementById('create-workspace-popup')
   document.body.removeChild(popup)
   document.removeEventListener('keyup', onKeyUp)
+  directory = ''
+  targetURL = ''
   reject()
 }
 
@@ -92,10 +133,16 @@ function onClickWrapper (e) {
 
 function onSubmit (e) {
   e.preventDefault()
-  resolve({
-    name: e.target.url.name,
-    url: e.target.url.value,
-    path: e.target.path.files[0].path
-  })
-  destroy()
+  if (!targetURL) {
+    document.querySelector('#create-workspace-popup .error').innerText = 'Please choose a target Dat archive'
+  } else if (!directory) {
+    document.querySelector('#create-workspace-popup .error').innerText = 'Please choose a directory'
+  } else {
+    resolve({
+      name: e.target.name.value,
+      url: targetURL,
+      path: e.target.path.files[0].path
+    })
+    destroy()
+  }
 }
