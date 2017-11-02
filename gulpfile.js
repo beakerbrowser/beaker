a2632cc2379d677303364028321700b7cf7b847c
Paul Frazee <pfrazee@gmail.com>
2017-11-02 11:33:41 -0500

    Add fix for osx build output names

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
-require('./tasks/build/build');
-require('./tasks/start');
+require('./tasks/build/build')
+require('./tasks/start')
 require('./tasks/rebuild')
+require('./tasks/postbuild')
---
+++ tasks/postbuild.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp')
+const jetpack = require('fs-jetpack')
+const run = require('./util-run')
+
+gulp.task('postbuild', async () => {
+  // OSX - for some reason, electron-builder is spitting out 'Beaker Browser-{version}{ext}'
+  // but the auto updater expects 'beaker-browser-{version}{ext}'
+  // so auto-rename any files like that
+
+  const cwd = jetpack.cwd('dist')
+  const names = await cwd.listAsync()
+  await Promise.all(names.map(name => {
+    if (name.startsWith('Beaker Browser')) {
+      const newName = 'beaker-browser' + name.slice('Beaker Browser'.length)
+      return cwd.move(name, newName)
+    }
+  }))
+})
