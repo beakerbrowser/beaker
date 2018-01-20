6e9befeb94c515f37fe1e10f5b1ac29cc3f6ee64
Tara Vancil <tbvanc@gmail.com>
2018-01-19 22:18:47 -0600

    Set up basic tools for working with workspaces in library

--- app/builtin-pages/views/library-view.js
+++ app/builtin-pages/views/library-view.js
@@ -12,10 +12,19 @@ import {writeToClipboard} from '../../lib/fg/event-handlers'
 // =
 
 var activeView = 'files'
+var activeMode = 'live'
 var archive
 var archiveInfo
 var archiveFsRoot
 var filesBrowser
+
+var workspaceInfo
+let diff
+let diffAdditions = 0
+let diffDeletions = 0
+let currentDiffNode
+let numCheckedRevisions
+
 var error
 
 // main
@@ -41,7 +50,47 @@ async function setup () {
   //   error = e
   // }
 
+  // fetch workspace info for this archive
+  workspaceInfo = await beaker.workspaces.get(0, archiveInfo.url)
+  if (workspaceInfo && workspaceInfo.localFilesPath) {
+    workspaceInfo.revisions = await beaker.workspaces.listChangedFiles(
+      0,
+      workspaceInfo.name,
+      {shallow: true, compareContent: true}
+    )
+  } else {
+    workspaceInfo.revisions = []
+  }
   render()
+}
+
+async function loadCurrentDiff (revision) {
+  if (!revision) {
+    diff = ''
+    currentDiffNode = null
+    diffAdditions = 0
+    diffDeletions = 0
+    return
+  }
+
+  // fetch the diff
+  try {
+    diff = await beaker.workspaces.diff(0, currentWorkspaceName, revision.path)
+
+    diffDeletions = diff.reduce((sum, el) => {
+      if (el.removed) return sum + el.count
+      return sum
+    }, 0)
+
+    diffAdditions = diff.reduce((sum, el) => {
+      if (el.added) return sum + el.count
+      return sum
+    }, 0)
+  } catch (e) {
+    if (e.invalidEncoding) {
+      diff = {invalidEncoding: true}
+    }
+  }
 }
 
 // rendering
@@ -56,6 +105,7 @@ function render () {
             <div class="container">
               ${renderInfo()}
               ${renderTabs()}
+              ${renderModeToggle()}
             </div>
           </div>
 
@@ -74,6 +124,8 @@ function renderView () {
       return renderFilesView()
     case 'settings':
       return renderSettingsView()
+    case 'revisions':
+      return renderRevisionsView()
     default:
       return yo`<div class="view">Loading...</div>`
   }
@@ -94,6 +146,122 @@ function renderSettingsView () {
     </div>
   `
 }
+
+function renderRevisionsView () {
+  if (!workspaceInfo.revisions.length) {
+    return yo`
+      <div class="view">
+        <em>No unpublished revisions</em>
+      </div>
+    `
+  }
+
+  const additions = workspaceInfo.revisions.filter(r => r.change === 'add')
+  const modifications = workspaceInfo.revisions.filter(r => r.change === 'mod')
+  const deletions = workspaceInfo.revisions.filter(r => r.change === 'del')
+
+  const renderRev = node => (
+    yo`
+      <li class="${currentDiffNode && node.path === currentDiffNode.path ? 'selected' : ''}" onclick=${() => onClickChangedNode(node)} title=${node.path}>
+        <code class="path">${node.type === 'file' ? node.path.slice(1) : node.path}</code>
+        <input
+          type="checkbox"
+          checked=${!!node.checked}
+          onclick=${e => onToggleChangedNodeChecked(e, node)}
+        />
+      </li>
+    `
+  )
+
+  return yo`
+    <div class="view revisions">
+      <div class="revisions-sidebar">
+        ${renderMetadata()}
+
+        ${additions.length ? yo`
+          <div>
+            <div class="revisions-header additions">
+              <h3>Additions</h3>
+              <span class="count">${additions.length}</span>
+            </div>
+
+            <ul class="revisions-list">
+              ${additions.map(renderRev)}
+            </ul>
+          </div>
+        ` : ''}
+
+        ${modifications.length ? yo`
+          <div>
+            <div class="revisions-header modifications">
+              <h3>Modifications</h3>
+              <span class="count">${modifications.length}</span>
+            </div>
+
+            <ul class="revisions-list">
+              ${modifications.map(renderRev)}
+            </ul>
+          </div>
+        ` : ''}
+
+        ${deletions.length ? yo`
+          <div>
+            <div class="revisions-header deletions">
+              <h3>Deletions</h3>
+              <span class="count">${deletions.length}</span>
+            </div>
+
+            <ul class="revisions-list">
+              ${deletions.map(renderRev)}
+            </ul>
+          </div>
+        ` : ''}
+        ${!(additions.length || modifications.length || deletions.length)
+          ? yo`<em>No revisions</em>`
+          : ''}
+
+        ${renderActions()}
+      </div>
+
+      <div class="revisions-content">
+        ${currentDiffNode ? yo`
+          <div class="revisions-content-header">
+            <div>
+              <i class="fa fa-file-text-o"></i>
+              <code class="path">
+                ${currentDiffNode.type === 'file' ? currentDiffNode.path.slice(1) : currentDiffNode.path}
+              </code>
+            </div>
+
+            <div class="changes-count-container">
+              <span class="additions-count">${diffAdditions ? `+${diffAdditions}` : ''}</span>
+              <span class="deletions-count">${diffDeletions ? `-${diffDeletions}` : ''}</span>
+          </div>` : ''}
+
+        ${renderRevisionsContent()}
+      </div>
+    </div>
+  `
+}
+
+function renderRevisionsContent () {
+  if (diff && diff.invalidEncoding) {
+    return yo`
+      <div class="binary-diff-placeholder">
+        <code>
+          1010100111001100
+          1110100101110100
+          1001010100010111
+        </code>
+      </div>
+    `
+  } else if (diff) {
+    return renderDiff(diff)
+  } else {
+    return 'butt'
+  }
+}
+
 
 function renderInfo () {
   return yo`
@@ -123,6 +291,22 @@ function renderTabs () {
         Files
       </div>
 
+      ${workspaceInfo
+        ? yo`
+          <div onclick=${e => onChangeView('revisions')} class="tab ${activeView === 'revisions' ? 'active' : ''}">
+            Revisions
+            ${workspaceInfo.revisions && workspaceInfo.revisions.length
+              ? yo`<span class="revisions-indicator"></span>`
+              : ''
+            }
+          </div>`
+        : ''
+      }
+
+      <div onclick=${e => onChangeView('network')} class="tab ${activeView === 'network' ? 'active' : ''}">
+        Network
+      </div>
+
       <div onclick=${e => onChangeView('settings')} class="tab ${activeView === 'settings' ? 'active' : ''}">
         <i class="fa fa-cogs"></i>
         Settings
@@ -131,7 +315,30 @@ function renderTabs () {
   `
 }
 
+function renderModeToggle () {
+  if (!workspaceInfo) return ''
+
+  return yo`
+    <div class="btn-group mode-toggle">
+      <button onclick=${() => onChangeMode('preview')} class="btn small mode ${activeMode === 'preview' ? 'active' : ''}">
+        Preview
+      </button>
+      <button onclick=${() => onChangeMode('live')} class="btn small mode ${activeMode === 'live' ? 'active' : ''}">
+        Live
+      </button>
+    </div>
+  `
+}
+
 function renderMetadata () {
+  let url = archiveInfo.url
+  let urlLabel = shortenHash(archiveInfo.url)
+
+  if (activeMode === 'preview' && workspaceInfo) {
+    url = `workspace://${workspaceInfo.name}`
+    urlLabel =  url
+  }
+
   return yo`
     <div class="metadata">
       <div>${prettyBytes(archiveInfo.size)}</div>
@@ -143,22 +350,40 @@ function renderMetadata () {
       <span class="separator">―</span>
 
       <div class="url-info">
-        <a href=${archiveInfo.url} class="url">
-          ${shortenHash(archiveInfo.url)}
-        </a>
+        <a href=${url} class="url" target="_blank">${urlLabel}</a>
       </div>
     </div>
   `
-
 }
 
 function renderActions () {
   return yo`
     <div class="actions">
-      <button class="btn transparent">
+      ${renderEditButton()}
+
+      <button class="btn">
         <i class="fa fa-ellipsis-v"></i>
       </button>
     </div>
+  `
+}
+
+function renderEditButton () {
+  if (workspaceInfo && workspaceInfo.localFilesPath) return ''
+
+  if (archiveInfo.isOwner) {
+    return yo`
+      <button class="btn" onclick=${onEdit}>
+        <i class="fa fa-pencil"></i>
+        Edit
+      </button>
+    `
+  }
+  return yo`
+    <button class="btn" onclick=${onForkAndEdit}>
+      <i class="fa fa-pencil"></i>
+      Fork & Edit
+    </button>
   `
 }
 
@@ -194,6 +419,19 @@ function onCopyUrl () {
     writeToClipboard(archiveInfo.url)
     toast.create('URL copied to clipboard')
   }
+}
+
+function onEdit () {
+  // TODO
+}
+
+function onForkAndEdit () {
+  // TODO
+}
+
+async function onCreateWorkspace () {
+  workspaceInfo = await beaker.workspaces.create(0)
+  render()
 }
 
 function onPopState (e) {
