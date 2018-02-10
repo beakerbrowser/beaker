011dfd1e2a610d55a51504cebffdf96724ac64a0
Paul Frazee <pfrazee@gmail.com>
2018-02-09 18:17:25 -0600

    Add live-reload tip

--- app/lib/i18n/help-tips.js
+++ app/lib/i18n/help-tips.js
@@ -8,5 +8,6 @@ export const EN = [
   'You can integrate Git to collaborate on sites and apps.',
   'You can use community apps to build your websites!',
   'The <a href="#">Library</a> contains all your bookmarks, sites, and seeded files.',
-  'Keep your sites online and get nicer URLs using <a href="https://hashbase.io" title="Hashbase.io">Hashbase.io</a>.'
+  'Keep your sites online and get nicer URLs using <a href="https://hashbase.io" title="Hashbase.io">Hashbase.io</a>.',
+  'Working on a site? Live reloading will help you dev faster!'
 ]
