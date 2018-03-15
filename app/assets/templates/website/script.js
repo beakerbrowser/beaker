d625a8b167eb559a1e7732538843ffdbecbc832b
Tara Vancil <tbvanc@gmail.com>
2018-03-15 12:35:38 -0500

    Use feature detecting to set the beakerbrowser.com URL in our default website template

--- app/assets/templates/website/script.js
+++ app/assets/templates/website/script.js
@@ -25,6 +25,14 @@ button.addEventListener('click', changeBackgroundColor)
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('url').innerText = window.location
   document.getElementById('library-url').href = `beaker://library/${window.location}`
+
+  // is this a Dat-supported website? If so, use a dat:// URL for the beakerbrowser.com link
+  const beakerLink = document.getElementById('beaker-url')
+  if (window.DatArchive) {
+    beakerLink.href = 'dat://beakerbrowser.com'
+  } else {
+    beakerLink.href = 'https://beakerbrowser.com'
+  }
 })
 
 function changeBackgroundColor () {
