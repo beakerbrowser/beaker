3599637542d8c8f893114d339941ca23fab739e0
Daksh Miglani <hello@dak.sh>
2018-04-22 07:52:17 +0530

    now the bug fixes are done, it does execute the script

--- app/json-renderer.js
+++ app/json-renderer.js
@@ -1,7 +1,7 @@
 import createJSON from './lib/fg/json'
 
 const isJSON = function (str) {
-  if (str.blank()) return false
+  if (str === "" || str === ' ') return false
   str = str.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@')
   str = str.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
   str = str.replace(/(?:^|:|,)(?:\s*\[)+/g, '')
