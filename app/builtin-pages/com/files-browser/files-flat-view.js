703354c706a29f5dae2a36d3054dd73839b67d59
Tara Vancil <tbvanc@gmail.com>
2018-05-24 14:11:40 -0500

    Don't show anything in actions area if previewing read-only

--- app/builtin-pages/com/files-browser/files-flat-view.js
+++ app/builtin-pages/com/files-browser/files-flat-view.js
@@ -180,7 +180,7 @@ function rFilePreview (filesBrowser, node) {
                     </button>
                   `
                 ]
-              : yo`<em>Read-only</em>`
+              : ''
             : ''}
           <a href=${node.url} target="_blank" class="tooltip-container" data-tooltip="Open file">
             <i class="fa fa-external-link"></i>
