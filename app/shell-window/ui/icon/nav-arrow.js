66aafed4c59c7da74cc5ce3d528b7b79519fbf1f
Paul Frazee <pfrazee@gmail.com>
2018-07-14 14:09:22 -0500

    Improve shell-window SVG rendering performance

--- app/shell-window/ui/icon/nav-arrow.js
+++ app/shell-window/ui/icon/nav-arrow.js
@@ -1,11 +1,21 @@
 import * as svg from '../../../lib/fg/svg'
 
 export default function render () {
-  return svg.render(`
-    <svg class="icon nav-arrow" width="9px" height="16px" viewBox="0 0 9 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linejoin="round">
-        <polyline id="nav-arrow" stroke="#000000" stroke-width="2" transform="translate(4.500000, 8.000000) scale(-1, 1) translate(-4.500000, -8.000000) " points="8 1 1 8 8 15"/>
-      </g>
-    </svg>
-  `)
+  // TODO
+  // this was replaced with svg.create() for performance
+  // keeping this commented for reference
+  // -prf
+  // return svg.render(`
+  //   <svg class="icon nav-arrow" width="9px" height="16px" viewBox="0 0 9 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  //     <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linejoin="round">
+  //       <polyline id="nav-arrow" stroke="#000000" stroke-width="2" transform="translate(4.500000, 8.000000) scale(-1, 1) translate(-4.500000, -8.000000) " points="8 1 1 8 8 15"/>
+  //     </g>
+  //   </svg>
+  // `)
+  return svg.create('#svg-nav-arrow', {
+    cls: 'icon nav-arrow',
+    width: 9,
+    height: 16,
+    viewBox: '0 0 9 16'
+  })
 }
