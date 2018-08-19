d2eacd482c224e9ddcb8b8ec3f4105d173c101e5
Paul Frazee <pfrazee@gmail.com>
2018-08-18 20:35:46 -0500

    Bump electron@2.0.7

--- tasks/rebuild.js
+++ tasks/rebuild.js
@@ -2,14 +2,14 @@ var gulp = require('gulp')
 var path = require('path')
 var run = require('./util-run')
 
-//(cd app && npm rebuild --runtime=electron --target=2.0.6 --disturl=https://atom.io/download/atom-shell --build-from-source); gulp build
+//(cd app && npm rebuild --runtime=electron --target=2.0.7 --disturl=https://atom.io/download/atom-shell --build-from-source); gulp build
 
 gulp.task('rebuild', gulp.series(function () {
   // TODO read electron version
   var cwd = path.join(process.cwd(), 'app')
   console.log(cwd)
   return new Promise(resolve => {
-    run(`npm rebuild --runtime=electron --target=2.0.6 --disturl=https://atom.io/download/atom-shell --build-from-source`, {cwd, shell: true}, function () {
+    run(`npm rebuild --runtime=electron --target=2.0.7 --disturl=https://atom.io/download/atom-shell --build-from-source`, {cwd, shell: true}, function () {
       run(`npm run build`, {shell: true}, function () {
         resolve()
       })
