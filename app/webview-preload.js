a6407a98b0680196a0331c6c9c75ce540325816b
Paul Frazee <pfrazee@gmail.com>
2018-09-18 19:03:38 -0500

    Remove deprecated scheme registration

--- app/webview-preload.js
+++ app/webview-preload.js
@@ -1,6 +1,5 @@
 import * as rpcAPI from 'pauls-electron-rpc'
 import * as beakerCoreWebview from '@beaker/core/webview'
-import { webFrame } from 'electron'
 import { setup as setupLocationbar } from './webview-preload/locationbar'
 import { setup as setupPrompt } from './webview-preload/prompt'
 import setupRedirectHackfix from './webview-preload/redirect-hackfix'
@@ -9,15 +8,6 @@ import setupExitFullScreenHackfix from './webview-preload/exit-full-screen-hackf
 // HACKS
 setupRedirectHackfix()
 setupExitFullScreenHackfix()
-
-// register protocol behaviors
-/* This marks the scheme as:
- - Secure
- - Allowing Service Workers
- - Supporting Fetch API
- - CORS Enabled
-*/
-webFrame.registerURLSchemeAsPrivileged('dat', { bypassCSP: false })
 
 beakerCoreWebview.setup({rpcAPI})
 setupLocationbar()
