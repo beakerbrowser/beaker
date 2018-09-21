308f29573efce91ac6fdfe834262f29d32a7ef62
Paul Frazee <pfrazee@gmail.com>
2018-09-21 17:23:15 -0500

    Restore registerURLSchemeAsPrivileged (turns out we still need that)

--- app/webview-preload.js
+++ app/webview-preload.js
@@ -1,8 +1,18 @@
+import {webFrame} from 'electron'
 import * as rpcAPI from 'pauls-electron-rpc'
 import * as beakerCoreWebview from '@beaker/core/webview'
 import { setup as setupLocationbar } from './webview-preload/locationbar'
 import { setup as setupPrompt } from './webview-preload/prompt'
 import setupExitFullScreenHackfix from './webview-preload/exit-full-screen-hackfix'
+
+// register protocol behaviors  
+/* This marks the scheme as:  
+ - Secure 
+ - Allowing Service Workers 
+ - Supporting Fetch API 
+ - CORS Enabled 
+*/  
+webFrame.registerURLSchemeAsPrivileged('dat', { bypassCSP: false })
 
 // HACKS
 setupExitFullScreenHackfix()
