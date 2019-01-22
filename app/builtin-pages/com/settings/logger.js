f4ce2917fc87d1b6f1e68698e0fc2ca7478262f0
Paul Frazee <pfrazee@gmail.com>
2019-01-21 21:18:43 -0600

    Add custom rules and persistent settings to log viewer

--- app/builtin-pages/com/settings/logger.js
+++ app/builtin-pages/com/settings/logger.js
@@ -8,6 +8,7 @@ import {writeToClipboard} from '../../../lib/fg/event-handlers'
 import _debounce from 'lodash.debounce'
 
 const AVAILABLE_LEVELS = ['error', 'warn', 'info', 'verbose', 'debug', 'silly']
+const AVAILABLE_CATEGORIES = ['all', 'crawler', 'dat']
 
 // globals
 // =
@@ -20,12 +21,23 @@ var idCounter = 0
 export default class Logger {
   constructor () {
     this.id = (++idCounter)
+
     this.isLoading = false
+    this.isControlsExpanded = false
+
     this.rows = null
     this.readStream = null
-    this.filter = {
-      level: AVAILABLE_LEVELS.slice(0, -1)
+
+    try {
+      this.settings = JSON.parse(localStorage.settings)
+    } catch (e) {
+      this.settings = {
+        level: AVAILABLE_LEVELS.slice(0, -2),
+        category: 'all',
+        customRules: []
+      }
     }
+
     this.isPaused = false
     this.pauseTime = undefined
   }
@@ -36,12 +48,16 @@ export default class Logger {
   async load () {
     this.isLoading = true
     this.rerenderControls()
-    this.rows = await beaker.logger.query({limit: 5e3, filter: this.filter, until: this.pauseTime, sort: 'desc'})
+    var filter = {level: this.settings.level}
+    if (this.settings.category !== 'all') filter.category = this.settings.category
+    this.rows = await beaker.logger.query({limit: 5e5, filter, until: this.pauseTime, sort: 'desc'})
+    this.rows = this.rows.filter(row => this.applyCustomRules(row))
 
     if (this.readStream) this.readStream.close()
     if (!this.isPaused) {
-      this.readStream = beaker.logger.stream({since: Date.now(), filter: this.filter})
+      this.readStream = beaker.logger.stream({since: Date.now(), filter})
       this.readStream.addEventListener('data', row => {
+        if (!this.applyCustomRules(row)) return
         this.rows.unshift(row)
         this.prependRowEl(this.renderRow(row, this.rows.length))
       })
@@ -49,6 +65,34 @@ export default class Logger {
 
     this.isLoading = false
     this.rerender()
+  }
+
+  applyCustomRules (row) {
+    if (this.settings.customRules.length === 0) {
+      return true
+    }
+
+    var ret = true
+    var rowJson = renderObject(row).toLowerCase()
+    for (let filter of this.settings.customRules) {
+      let isMatch = rowJson.indexOf(filter.query.toLowerCase()) !== -1
+      if (isMatch) {
+        if (filter.effect === 'exclude-matches') {
+          ret = false
+        } else if (filter.effect.startsWith('highlight-')) {
+          row.highlight = filter.effect.slice('highlight-'.length)
+        }
+      } else {
+        if (filter.effect === 'exclude-nonmatches') {
+          ret = false
+        }
+      }
+    }
+    return ret
+  }
+
+  saveSettings () {
+    localStorage.settings = JSON.stringify(this.settings)
   }
 
   // rendering
@@ -94,29 +138,63 @@ export default class Logger {
   }
 
   renderControls () {
+    const option = (curr, v, label) => {
+      return yo`<option value="${v}" ${curr === v ? 'selected' : ''}>${label}</option>`
+    }
     return yo`
       <div class="controls">
-        <span>
-          <button class="btn transparent" onclick=${() => this.onTogglePaused()}>
-            <i class="fa fa-${this.isPaused ? 'play' : 'pause'}"></i>
-            ${this.isPaused ? 'Resume' : 'Pause'}
+        <div class="standard-controls">
+          <button class="btn transparent" onclick=${() => this.onToggleControlsExpanded()}>
+            <i class="fas fa-${this.isControlsExpanded ? 'angle-down' : 'angle-up'}"></i>
           </button>
-        </span>
-        <span class="divider thin"></span>
-        ${AVAILABLE_LEVELS.map(level => this.renderLevelFilter(level))}
-        <span class="divider"></span>
-        <span class="status">
-          ${this.isLoading
-            ? 'Loading...'
-            : this.isPaused
-              ? 'Paused'
-              : 'Streaming'}
-        </span>
+          <span class="divider thin"></span>
+          ${AVAILABLE_LEVELS.map(level => this.renderLevelFilter(level))}
+          <span class="divider"></span>
+          ${AVAILABLE_CATEGORIES.map(category => this.renderCategoryFilter(category))}
+          <span class="divider"></span>
+          <span class="status">
+            <button class="btn transparent" onclick=${() => this.onTogglePaused()}>
+              <i class="fa fa-${this.isPaused ? 'play' : 'pause'}"></i>
+              ${this.isPaused ? 'Resume' : 'Pause'}
+            </button>
+            ${this.isLoading
+              ? '[ Loading... ]'
+              : this.isPaused
+                ? ''
+                : '[ Streaming ]'}
+          </span>
+        </div>
+        ${this.isControlsExpanded ? this.settings.customRules.map(rule => yo`
+          <div class="custom-filter-controls">
+            <input type="text" value=${rule.query} onchange=${e => this.onChangeCustomRuleQuery(e, rule)}>
+            <select onchange=${e => this.onChangeCustomRuleEffect(e, rule)}>
+              ${option(rule.effect, 'exclude-nonmatches', 'Exclude non-matches')}
+              ${option(rule.effect, 'exclude-matches', 'Exclude matches')}
+              ${option(rule.effect, 'highlight-green', 'Highlight Green')}
+              ${option(rule.effect, 'highlight-blue', 'Highlight Blue')}
+              ${option(rule.effect, 'highlight-cyan', 'Highlight Cyan')}
+              ${option(rule.effect, 'highlight-purple', 'Highlight Purple')}
+              ${option(rule.effect, 'highlight-yellow', 'Highlight Yellow')}
+              ${option(rule.effect, 'highlight-red', 'Highlight Red')}
+            </select>
+            <button class="btn transparent" onclick=${() => this.onRemoveCustomRule(rule)}>
+              <i class="fa fa-times"></i>
+            </button>
+          </div>
+        `) : ''}
+        ${this.isControlsExpanded
+          ? yo`
+            <div>
+              <button class="btn transparent" onclick=${() => this.onAddCustomRule()}>
+                <i class="fas fa-plus"></i> Add filter
+              </button>
+            </div>`
+          : ''}
       </div>`
   }
 
   renderLevelFilter (level) {
-    var isChecked = this.filter.level.includes(level)
+    var isChecked = this.settings.level.includes(level)
     return yo`
       <label>
         <input type="checkbox" checked=${isChecked ? 'checked' : ''} onchange=${() => this.onToggleLevelFilter(level)}>
@@ -125,9 +203,19 @@ export default class Logger {
     `
   }
 
+  renderCategoryFilter (category) {
+    var isChecked = this.settings.category === category
+    return yo`
+      <label>
+        <input type="checkbox" checked=${isChecked ? 'checked' : ''} onchange=${() => this.onSelectCategory(category)}>
+        <i class="far ${isChecked ? 'fa-check-circle' : 'fa-circle'}"></i> ${category}
+      </label>
+    `
+  }
+
   renderRow (row, i) {
     return yo`
-      <tr class="logger-row level-${row.level}" oncontextmenu=${e => this.onContextmenuRow(e, row)}>
+      <tr class="logger-row ${row.highlight || ''}" oncontextmenu=${e => this.onContextmenuRow(e, row)}>
         <td class="level ${row.level}">${row.level}</td>
         <td class="category">${row.category}</td>
         <td class="subcategory">${row.subcategory || row.dataset}</td>
@@ -141,14 +229,54 @@ export default class Logger {
 
   onToggleLevelFilter (level) {
     // toggle the filter
-    if (this.filter.level.includes(level)) {
-      this.filter.level = this.filter.level.filter(l => l !== level)
+    if (this.settings.level.includes(level)) {
+      this.settings.level = this.settings.level.filter(l => l !== level)
     } else {
-      this.filter.level.push(level)
+      this.settings.level.push(level)
     }
+    this.saveSettings()
 
     // reload
     this.rerenderControls()
+    this.load()
+  }
+
+  onSelectCategory (category) {
+    this.settings.category = category
+    this.saveSettings()
+
+    // reload
+    this.rerenderControls()
+    this.load()
+  }
+
+  onToggleControlsExpanded () {
+    this.isControlsExpanded = !this.isControlsExpanded
+    this.rerenderControls()
+  }
+
+  onAddCustomRule () {
+    this.settings.customRules.push(new CustomRule())
+    this.saveSettings()
+    this.rerenderControls()
+  }
+
+  onRemoveCustomRule (rule) {
+    this.settings.customRules = this.settings.customRules.filter(f => f !== rule)
+    this.saveSettings()
+    this.rerenderControls()
+    this.load()
+  }
+
+  onChangeCustomRuleQuery (e, rule) {
+    rule.query = e.currentTarget.value
+    this.saveSettings()
+    this.load()
+  }
+
+  onChangeCustomRuleEffect (e, rule) {
+    rule.effect = e.currentTarget.value
+    this.saveSettings()
     this.load()
   }
 
@@ -200,13 +328,27 @@ export default class Logger {
   }
 }
 
-function renderDetails (obj) {
+function renderObject (obj) {
   var items = []
   for (let k in obj) {
     var v = obj[k]
     if (Array.isArray(v)) v = `[${v.join(',')}]`
-    if (typeof v === 'object') v = JSON.stringify(v)
+    if (typeof v === 'object') v = `{${renderObject(v)}}`
     items.push(`${k}=${v}`)
   }
-  return yo`<small>(${items.join(' ')})</small>`
+  return items.join(' ')
+}
+
+function renderDetails (obj) {
+  return yo`<small>${renderObject(obj)}</small>`
+}
+
+// internal
+// =
+
+class CustomRule {
+  constructor () {
+    this.query = ''
+    this.effect = 'exclude-nonmatches'
+  }
 }
