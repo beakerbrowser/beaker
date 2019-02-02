15326b14054da7a9847f31cf25c60ea484d9053f
Paul Frazee <pfrazee@gmail.com>
2019-02-02 17:01:41 -0600

    Only include macos flags to compiler on darwin platform

--- tasks/rebuild.js
+++ tasks/rebuild.js
@@ -9,10 +9,13 @@ gulp.task('rebuild', gulp.series(function () {
   var cwd = path.join(process.cwd(), 'app')
   console.log(cwd)
   return new Promise(resolve => {
-    var env = {
-      // required to make spellchecker compile
-      CXXFLAGS: '-mmacosx-version-min=10.10',
-      LDFLAGS: '-mmacosx-version-min=10.10'
+    var env = {}
+    if (process.platform === 'darwin') {
+      env = {
+        // required to make spellchecker compile
+        CXXFLAGS: '-mmacosx-version-min=10.10',
+        LDFLAGS: '-mmacosx-version-min=10.10'
+      }
     }
     run(`npm rebuild --runtime=electron --target=4.0.3 --disturl=https://atom.io/download/atom-shell --build-from-source`, {cwd, env, shell: true}, function () {
       run(`npm run build`, {shell: true}, function () {
