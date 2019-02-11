d7cc8f6e14f5f93ba78e7757558af5d59dc3c58b
Paul Frazee <pfrazee@gmail.com>
2019-02-11 08:59:59 -0600

    Fix: dont let an uncaught exception take down the dat daemon

--- app/dat-daemon.js
+++ app/dat-daemon.js
@@ -2,6 +2,10 @@ const {join} = require('path')
 const rpcAPI = require('pauls-electron-rpc')
 const beakerCoreDatDaemon = require('@beaker/core/dat/daemon')
 
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err)
+})
+
 process.once('message', firstMsg => {
   beakerCoreDatDaemon.setup({
     rpcAPI,
