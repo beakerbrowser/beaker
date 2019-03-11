f2cf098e0d1f537531d7888373aa6200f703d1bb
Paul Frazee <pfrazee@gmail.com>
2019-03-11 17:27:57 -0500

    Editor: disable toolbar on readonly

--- app/builtin-pages/com/editor/toolbar.js
+++ app/builtin-pages/com/editor/toolbar.js
@@ -8,15 +8,16 @@ export function render (file, model, opts) {
   if (!model) {
     return yo`<div class="editor-toolbar"></div>`
   }
+  var isEditable = file && file.isEditable
   var change = file && file.change
   return yo`
     <div class="editor-toolbar">
-      <span class="btn transparent" onclick=${e => onClickNewFile(e)}><i class="far fa-file"></i> new file</span>
+      <span class="btn transparent ${isEditable ? '' : 'disabled'}" onclick=${isEditable ? e => onClickNewFile(e) : undefined}><i class="far fa-file"></i> new file</span>
       <span class="divider"></span>
-      <span class="btn transparent ${model.isDirty? '' : 'disabled'}"  onclick=${e => onClickSaveFile(e, model)}><i class="fas fa-save"></i> save</span>
+      <span class="btn transparent ${model.isDirty ? '' : 'disabled'}"  onclick=${e => onClickSaveFile(e, model)}><i class="fas fa-save"></i> save</span>
       <span class="btn transparent" onclick=${e => onClickOpenFile(e, model)}><i class="fas fa-external-link-alt"></i> view file</span>
-      <span class="btn transparent" onclick=${e => onClickRenameFile(e, model)}><i class="fas fa-i-cursor"></i> rename</span>
-      <span class="btn transparent" onclick=${e => onClickDeleteFile(e, model)}><i class="fas fa-trash"></i> delete</span>
+      <span class="btn transparent ${isEditable ? '' : 'disabled'}" onclick=${isEditable ? e => onClickRenameFile(e, model) : undefined}><i class="fas fa-i-cursor"></i> rename</span>
+      <span class="btn transparent ${isEditable ? '' : 'disabled'}" onclick=${isEditable ? e => onClickDeleteFile(e, model) : undefined}><i class="fas fa-trash"></i> delete</span>
       ${opts.previewMode
         ? [
           yo`<span class="divider"></span>`,
