deb4f705a82f8ef2ec95a1fa8677891e7df38b82
Paul Frazee <pfrazee@gmail.com>
2019-05-14 10:46:16 -0500

    Fix invalid reference

--- app/background-process/ui/view-manager.js
+++ app/background-process/ui/view-manager.js
@@ -753,7 +753,7 @@ export function findContainingWindow (browserView) {
     }
   }
   for (let winId in preloadedNewTabViews) {
-    if (preloadedNewTabViews[winId].browserView === view) {
+    if (preloadedNewTabViews[winId].browserView === browserView) {
       return preloadedNewTabViews[winId].browserWindow
     }
   }
