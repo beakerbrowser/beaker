1658b341b5908eb3ec22b5f4491637d4ef09a192
Rémi Berson <remi@cliqz.com>
2019-08-18 21:40:55 +0200

    feature: enable adblocker using @cliqz/adblocker-electron

--- app/background-process/adblocker.js
+++ app/background-process/adblocker.js
@@ -1,24 +1,30 @@
 import { session } from 'electron'
-// import { initialize, containsAds } from 'contains-ads'
+import { ElectronBlocker } from '@cliqz/adblocker-electron'
+import fetch from 'node-fetch'
 
 // exported API
 
 export function setup () {
-  /*
-  temporarily disabled due to https://github.com/bbondy/bloom-filter-cpp/issues/7
-    "contains-ads": "^0.2.5",
-
-  initialize() // contains-ads
-
-  const beakerUrls = /^(beaker|blob)/
-  session.defaultSession.webRequest.onBeforeRequest(['*://*./*'], (details, callback) => {
-    const shouldBeBlocked = !details.url.match(beakerUrls) && containsAds(details.url)
-
-    if (shouldBeBlocked) {
-      callback({cancel: true})
-    } else {
-      callback({cancel: false})
-    }
+  // Initializes an adblocker engine targeted at ads only (for extra tracking
+  // protection, `ElectronBlocker.fromPrebuiltAdsAndTracking(fetch)` can be
+  // used instead). The following lists are used with this preset:
+  //
+  // - Easylist: https://easylist.to/easylist/easylist.txt
+  // - Peter Lowe adservers list: https://pgl.yoyo.org/adservers/
+  // - uBlock Origin (resource abuse): https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt
+  // - uBlock Origin (filters): https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt
+  // - uBlock Origin (unbreak): https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt
+  //
+  // Currently the resources are downloaded (less than 2 MB of data) every time
+  // Beaker starts, but could be cached locally in the future.
+  ElectronBlocker.fromPrebuiltAdsOnly(fetch).then((blocker) => {
+    // Calling this function will perform the following actions in order to
+    // enable adblocking in Beaker:
+    //
+    // - register listener to `webRequest.onHeadersReceived`
+    // - register listener to `webRequest.onBeforeRequest`
+    // - register a content script with `setPreloads` which allows to block
+    //   some extra ads which cannot be blocked by network filtering alone
+    blocker.enableBlockingInSession(session.defaultSession)
   })
-  */
 }
