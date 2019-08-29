874749724fa6f3b432e52653a52c0cd759b2209e
Paul Frazee <pfrazee@gmail.com>
2019-08-29 09:34:14 -0500

    Remove some old code

--- app/background-process/browser.js
+++ app/background-process/browser.js
@@ -101,7 +101,6 @@ export async function setup () {
 
   // wire up events
   app.on('web-contents-created', onWebContentsCreated)
-  beakerCore.users.on('user-thumb-changed', onUserThumbChanged)
 
   // window.prompt handling
   //  - we have use ipc directly instead of using rpc, because we need custom
@@ -675,10 +674,6 @@ function onWebContentsCreated (e, webContents) {
     // do not allow
     e.preventDefault()
   })
-}
-
-function onUserThumbChanged (user) {
-  browserEvents.emit('user-thumb-changed', user)
 }
 
 function onWillPreventUnload (e) {
--- app/builtin-pages/views/settings.js
+++ app/builtin-pages/views/settings.js
@@ -7,8 +7,6 @@ import DatCache from '../com/settings/dat-cache'
 import CrawlerStatus from '../com/settings/crawler-status'
 import renderBuiltinPagesNav from '../com/builtin-pages-nav'
 
-const DEFAULT_APP_NAMES = ['start', 'feed', 'profile', 'library', 'bookmarks', 'search']
-
 // globals
 // =
 
