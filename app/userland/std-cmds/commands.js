ecd7a243afa7d9b1fa273a3d074e21ba1954bf0e
Paul Frazee <pfrazee@gmail.com>
2019-12-20 21:28:54 -0600

    Add missing commands.js

---
+++ app/userland/std-cmds/commands.js
@@ -0,0 +1,85 @@
+import { ensureDir, ensureMount, ensureUnmount, ensureUnmountByUrl, getAvailableName } from 'beaker://app-stdlib/js/fs.js'
+
+export async function ls () {
+  let res = await navigator.filesystem.query({path: '/settings/webterm/cmds/*'})
+  res = res.filter(r => r.stat.isDirectory())
+  res = res.map(r => ({name: r.path.split('/').pop(), path: r.path, url: r.mount}))
+  res.toHTML = () => {
+    return res.map(r => html`<p><a href=${navigator.filesystem.url + r.path}>${r.name}</a></p>`)
+  }
+  return res
+}
+
+export async function create () {
+  var title
+  var description
+  while (!title) title = await this.prompt('Package title')
+  description = await this.prompt('Package description')
+
+  var drive = await DatArchive.create({
+    type: 'webterm.sh/cmd-pkg',
+    title,
+    description,
+    prompt: false
+  })
+  await drive.writeFile('/index.json', JSON.stringify({
+    type: 'webterm.sh/cmd-pkg',
+    title,
+    description,
+    commands: STARTER_MANIFEST_CMDS
+  }, null, 2))
+  await drive.writeFile('/index.js', STARTER_INDEX_JS)
+
+  return install(undefined, drive.url)
+}
+
+export async function install (opts = {}, url) {
+  await ensureDir('/settings')
+  await ensureDir('/settings/webterm')
+  await ensureDir('/settings/webterm/cmds')
+
+  var driveInfo = await (new DatArchive(url)).getInfo()
+  var name = await getAvailableName('/settings/webterm/cmds', driveInfo.title || 'untitled')
+  let path = `/settings/webterm/cmds/${name}`
+  await ensureMount(path, driveInfo.url)
+
+  return {name, path, url, toHTML: () => html`Installed <a href=${navigator.filesystem.url + path}>${name}</a>`}
+}
+
+export async function uninstall (opts = {}, urlOrName) {
+  await ensureDir('/settings')
+  await ensureDir('/settings/webterm')
+  await ensureDir('/settings/webterm/cmds')
+  
+  await ensureUnmount(`/settings/webterm/cmds/${urlOrName}`)
+  await ensureUnmountByUrl(`/settings/webterm/cmds/*`, urlOrName)
+}
+
+// internal
+// =
+
+const STARTER_MANIFEST_CMDS = [
+  {
+    "name": "example",
+    "help": "An example command",
+    "usage": "example [-m] {arg1} {arg2}",
+    "options": [
+      {
+        "name": "myswitch",
+        "abbr": "m",
+        "help": "An example switch",
+        "boolean": true,
+        "default": false
+      }
+    ]
+  }
+]
+
+const STARTER_INDEX_JS = `
+export async function example (opts = {}, arg1, arg2) {
+  if (opts.myswitch) {
+    this.out('My switch was included')
+  }
+  return arg1 + ' ' + arg2
+}
+`
