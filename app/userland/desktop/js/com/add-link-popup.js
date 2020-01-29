f1c2a8030461136399c4d8d7f3e85d1f1ccac8e4
Paul Frazee <pfrazee@gmail.com>
2020-01-29 11:45:39 -0600

    Remove files explorer from desktop add pin tooling

--- app/userland/desktop/js/com/add-link-popup.js
+++ app/userland/desktop/js/com/add-link-popup.js
@@ -6,9 +6,7 @@ import buttonsCSS from 'beaker://app-stdlib/css/buttons2.css.js'
 import popupsCSS from 'beaker://app-stdlib/css/com/popups.css.js'
 import { writeToClipboard } from 'beaker://app-stdlib/js/clipboard.js'
 import * as contextMenu from 'beaker://app-stdlib/js/com/context-menu.js'
-import { toNiceUrl, joinPath, normalizeUrl } from 'beaker://app-stdlib/js/strings.js'
-import { toSimpleItemGroups, getSubicon } from 'beaker://explorer/js/lib/files.js'
-import 'beaker://explorer/js/com/folder/file-grid.js'
+import { toNiceUrl, normalizeUrl } from 'beaker://app-stdlib/js/strings.js'
 
 // exported api
 // =
@@ -78,11 +76,6 @@ export class AddLinkPopup extends BasePopup {
       color: #667;
     }
 
-    file-grid {
-      display: block;
-      padding: 0 16px 0;
-    }
-
     .history {
       margin-top: 0 !important;
     }
@@ -90,29 +83,6 @@ export class AddLinkPopup extends BasePopup {
     .empty {
       color: rgba(0, 0, 0, 0.5);
       padding: 20px;
-    }
-
-    file-grid.empty {
-      padding: 0 20px 20px;
-    }
-
-    .group {
-      padding: 0 0 20px;
-    }
-
-    .group-title {
-      border-bottom: 1px solid rgba(0, 0, 0, 0.25);
-      color: rgba(0, 0, 0, 0.85);
-      margin-bottom: 10px;
-      padding-bottom: 5px;
-      padding-left: 20px;
-      letter-spacing: -0.5px;
-    }
-
-    .group-items {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      padding-right: 20px;
     }
 
     .suggestion {
@@ -167,8 +137,6 @@ export class AddLinkPopup extends BasePopup {
 
   constructor () {
     super()
-    this.explorerPath = '/'
-    this.explorerItems = []
     this.query = ''
     this.history = []
     this.selection = []
@@ -193,54 +161,28 @@ export class AddLinkPopup extends BasePopup {
 
   reset () {
     this.selection = []
-    this.explorerItems = []
     this.history = []
   }
 
   async load () {
     this.reset()
-
-    var explorerItems = await navigator.filesystem.readdir(this.explorerPath, {includeStats: true})
-    var driveInfo = await navigator.filesystem.getInfo()
-    for (let item of explorerItems) {
-      item.drive = driveInfo
-      item.path = joinPath(this.explorerPath, item.name)
-      item.url = joinPath(navigator.filesystem.url, item.path)
-      item.realPath = item.path
-      item.realUrl = joinPath(item.drive.url, item.realPath)
-      if (item.stat.mount && item.stat.mount.key) {
-        item.mount = await (new Hyperdrive(item.stat.mount.key)).getInfo()
-      }
-      item.icon = item.stat.isDirectory() ? 'folder' : 'file'
-      if (item.stat.isFile() && item.name.endsWith('.view')) {
-        item.icon = 'layer-group'
-      } else if (item.stat.isFile() && item.name.endsWith('.goto')) {
-        item.icon = 'external-link-alt'
-      }
-      item.subicon = getSubicon('root', item)
-    }
-    explorerItems.sort((a, b) => a.name.localeCompare(b.name))
-    
-    this.explorerItems = explorerItems
-    this.requestUpdate()
+    this.runQuery()
   }
 
   async runQuery () {
     this.reset()
-    this.history = await beaker.history.search(this.query)
-    var queryEntry = normalizeUrl(this.query)
-    if (!this.history.find(item => normalizeUrl(item.url) === queryEntry)) {
-      this.history.unshift({
-        title: queryEntry,
-        url: queryEntry
-      })
+    this.history = await (this.query ? beaker.history.search(this.query) : beaker.history.getVisitHistory(0))
+    if (this.query) {
+      let queryEntry = normalizeUrl(this.query)
+      if (!this.history.find(item => normalizeUrl(item.url) === queryEntry)) {
+        this.history.unshift({
+          title: queryEntry,
+          url: queryEntry
+        })
+      }
     }
     console.log(this.history)
     this.requestUpdate()
-  }
-
-  get explorerItemGroups () {
-    return toSimpleItemGroups(this.explorerItems)
   }
 
   // rendering
@@ -258,29 +200,11 @@ export class AddLinkPopup extends BasePopup {
         <input type="text" id="search-input" name="url" placeholder="Search my history or input a URL" @keyup=${e => delay(this.onChangeQuery.bind(this), e)} />
       </div>
       <main>
-        ${this.query ? html`
-          <div class="history ${this.query ? 'query-results' : 'defaults'}">
-            ${hasResults ? '' : html`<div class="empty">No results</div>`}
-            ${repeat(this.history, item => this.renderHistoryItem(item))}
-          </div>
-        ` : html`
-          <nav>
-            <button @click=${this.onClickExplorerUpdog}><span class="fas fa-level-up-alt"></span></button>
-            <span class="path">${this.explorerPath.split('/').filter(Boolean).map(segment => html`<span>/</span><span>${segment}</span>`)}</span>
-          </nav>
-          <file-grid
-            class=${this.explorerItems.length === 0 ? 'empty' : ''}
-            .itemGroups=${this.explorerItemGroups}
-            .selection=${this.selection}
-            @goto=${this.onExplorerGoto}
-            @change-selection=${this.onExplorerChangeSelection}
-            @show-context-menu=${this.onExplorerContextMenu}
-          ></file-grid>
-        `}
+        <div class="history ${this.query ? 'query-results' : 'defaults'}">
+          ${hasResults ? '' : html`<div class="empty">No results</div>`}
+          ${repeat(this.history, item => this.renderHistoryItem(item))}
+        </div>
       </main>
-      <footer>
-        <button class="primary big" ?disabled=${this.selection.length === 0} @click=${this.onClickSubmit}>Add Selected Item</button>
-      </footer>
     `
   }
 
@@ -310,36 +234,6 @@ export class AddLinkPopup extends BasePopup {
     else this.load()
   }
 
-  onClickExplorerUpdog (e) {
-    this.explorerPath = this.explorerPath.split('/').slice(0, -1).join('/') || '/'
-    this.load()
-  }
-
-  onExplorerGoto (e) {
-    var {item} = e.detail
-    if (item.stat.isFile()) {
-      this.selection = [item]
-      return this.onClickSubmit(e)
-    }
-    this.explorerPath = item.path
-    this.load()
-  }
-
-  onExplorerChangeSelection (e) {
-    this.selection = e.detail.selection
-    this.requestUpdate()
-  }
-
-  async onExplorerContextMenu (e) {
-    if (!this.selection[0]) return
-    var url = await getUrl(this.selection[0])
-    const items = [
-      {icon: 'fa fa-external-link-alt', label: 'Open Link in New Tab', click: () => window.open(url)},
-      {icon: 'fa fa-link', label: 'Copy Link Address', click: () => writeToClipboard(url)}
-    ]
-    contextMenu.create({x: e.detail.x, y: e.detail.y, items, fontAwesomeCSSUrl: 'beaker://assets/font-awesome.css'})
-  }
-
   onClickHistory (e) {
     e.preventDefault()
     const detail = {
@@ -358,55 +252,11 @@ export class AddLinkPopup extends BasePopup {
     ]
     contextMenu.create({x: e.clientX, y: e.clientY, items, fontAwesomeCSSUrl: 'beaker://assets/font-awesome.css'})
   }
-
-  async onClickSubmit (e) {
-    e.preventDefault()
-    var sel = this.selection[0]
-    if (!sel) return
-    const detail = {
-      href: await getUrl(sel),
-      title: getTitle(sel)
-    }
-    this.dispatchEvent(new CustomEvent('resolve', {detail}))
-  }
 }
 customElements.define('add-link-popup', AddLinkPopup)
 
 // helpers
 // =
-
-async function getUrl (item) {
-  if (item.name.endsWith('.goto')) {
-    return item.stat.metadata.href
-  }
-  if (item.mountInfo) {
-    return item.mountInfo.url
-  }
-  return getRealUrl(item)
-}
-
-function getTitle (item) {
-  if (item.name.endsWith('.goto')) {
-    return item.stat.metadata.title || item.name
-  }
-  if (item.mountInfo) {
-    return item.mountInfo.title
-  }
-  return item.name
-}
-
-async function getRealUrl (item) {
-  var pathParts = item.path.split('/').filter(Boolean)
-  var discardedParts = []
-  while (pathParts.length > 0) {
-    let st = await navigator.filesystem.stat(pathParts.join('/'))
-    if (st.mount) {
-      return `hd://${st.mount.key}/${discardedParts.join('/')}`
-    }
-    discardedParts.unshift(pathParts.pop())
-  }
-  return item.url + (item.stat.isDirectory() ? '/' : '')
-}
 
 function delay (cb, param) {
   window.clearTimeout(cb)
