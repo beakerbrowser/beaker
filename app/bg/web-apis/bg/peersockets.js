cde9b733f61ad3ed93b750943c9a0f4d654bb123
Andrew Osheroff <andrewosh@gmail.com>
2020-07-22 01:46:06 +0200

    Update peersockets usage + move peer aliasing logic into Beaker

--- app/bg/web-apis/bg/peersockets.js
+++ app/bg/web-apis/bg/peersockets.js
@@ -3,6 +3,8 @@ import { getClient } from '../../hyper/daemon'
 import * as drives from '../../hyper/drives'
 import { PermissionsError } from 'beaker-error-constants'
 
+const sessionAliases = new Map()
+
 // exported api
 // =
 
@@ -10,6 +12,7 @@ export default {
   async join (topic) {
     var drive = await getSenderDrive(this.sender)
     topic = massageTopic(topic, drive.discoveryKey)
+    const aliases = getAliases(this.sender)
 
     var stream = new Duplex({
       write (data, cb) {
@@ -17,33 +20,40 @@ export default {
           console.debug('Incorrectly formed message from peersockets send API', data)
           return cb(null)
         }
-        topicHandle.send(data[0], data[1])
+        const peer = getPeerForAlias(aliases, data[0])
+        if (!peer) return
+        topicHandle.send(data[1], peer)
         cb(null)
       }
     })
     stream.objectMode = true
     var topicHandle = getClient().peersockets.join(topic, {
-      onmessage (peerId, message) {
-        stream.push(['message', {peerId, message}])
+      onmessage (message, peer) {
+        stream.push(['message', {peerId: createAliasForPeer(aliases, peer), message}])
       }
     })
     drive.pda.numActiveStreams++
     stream.on('close', () => {
       drive.pda.numActiveStreams--
+      releaseAliases(this.sender, aliases)
       topicHandle.close()
     })
-    
+
     return stream
   },
 
   async watch () {
     var drive = await getSenderDrive(this.sender)
+    const aliases = getAliases(this.sender)
     var stream = new Readable()
-    var stopwatch = getClient().peers.watchPeers(drive.discoveryKey, {
-      onjoin: async (peerId) => stream.push(['join', {peerId}]),
-      onleave: (peerId) => stream.push(['leave', {peerId}])
+    var stopwatch = getClient().peers.watchPeers(drive.key, {
+      onjoin: async (peer) => stream.push(['join', {peerId: createAliasForPeer(aliases, peer)}]),
+      onleave: (peer) => stream.push(['leave', {peerId: createAliasForPeer(aliases, peer) }])
     })
-    stream.on('close', () => stopwatch())
+    stream.on('close', () => {
+      releaseAliases(this.sender, aliases)
+      stopwatch()
+    })
     return stream
   }
 }
@@ -61,4 +71,31 @@ async function getSenderDrive (sender) {
 
 function massageTopic (topic, discoveryKey) {
   return `webapp/${discoveryKey.toString('hex')}/${topic}`
+}
+
+function getAliases (sender) {
+  let aliases = sessionAliases.get(sender)
+  if (!aliases) {
+    aliases = {refs: 0, byPeer: new Map(), byAlias: []}
+    sessionAliases.set(sender, aliases)
+  }
+  aliases.refs++
+  return aliases
+}
+
+function releaseAliases (sender, aliases) {
+  if (!--aliases.refs) sessionAliases.delete(sender)
+}
+
+function createAliasForPeer (aliases, peer) {
+  let alias = aliases.byPeer.get(peer)
+  if (alias) return alias
+  alias = aliases.byPeer.size + 1
+  aliases.byPeer.set(peer, alias)
+  aliases.byAlias[alias] = peer
+  return alias
+}
+
+function getPeerForAlias (aliases, alias) {
+  return aliases.byAlias[alias]
 }
