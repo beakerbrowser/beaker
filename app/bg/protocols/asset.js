be700dc6a2cdb57930310a7d801b1919906b87b9
Paul Frazee <pfrazee@gmail.com>
2020-10-07 18:01:00 -0500

    Add app icons

--- app/bg/protocols/asset.js
+++ app/bg/protocols/asset.js
@@ -68,6 +68,10 @@ export function setup () {
       let name = /beaker:\/\/([^\/]+)/.exec(url)[1]
       return servePng(path.join(__dirname, `./assets/img/favicons/${name}.png`), DEFAULTS[asset], cb)
     }
+    if (asset === 'screenshot' && /beaker:\/\/(social|uplink|reader)/.test(url)) {
+      let name = /beaker:\/\/([^\/]+)/.exec(url)[1]
+      return servePng(path.join(__dirname, `./assets/img/thumbs/${name}.png`), DEFAULTS[asset], cb)
+    }
 
     try {
       // look up in db
