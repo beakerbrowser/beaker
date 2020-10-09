e6e4babb2ae70cda6fb5b3db63ef771bef3a8bb1
Paul Frazee <pfrazee@gmail.com>
2020-10-08 20:17:51 -0500

    Delete app-stdlib (changing to a git subtree)

--- app/userland/app-stdlib/js/clipboard.js
+++
@@ -1,8 +0,0 @@
-export function writeToClipboard (str) {
-  var textarea = document.createElement('textarea')
-  textarea.textContent = str
-  document.body.appendChild(textarea)
-  textarea.select()
-  document.execCommand('copy')
-  document.body.removeChild(textarea)
-}
