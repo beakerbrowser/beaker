2490fb661474db9ecaeab696387d478be3f150a6
Paul Frazee <pfrazee@gmail.com>
2020-12-02 20:37:20 -0600

    Fix the patreon link in the default bookmarks

--- app/bg/filesystem/index.js
+++ app/bg/filesystem/index.js
@@ -80,14 +80,14 @@ export async function setup () {
   // default pinned bookmarks
   if (isInitialCreation) {
     await rootDrive.pda.mkdir('/bookmarks')
-    await rootDrive.pda.writeFile(`/bookmarks/patreon-com-andrew_maf_and_paul.goto`, '', {metadata: {href: 'https://patreon.com/andrew_maf_and_paul', title: 'Support Beaker'}})
+    await rootDrive.pda.writeFile(`/bookmarks/patreon-com-paul_maf_and_andrew.goto`, '', {metadata: {href: 'https://patreon.com/paul_maf_and_andrew', title: 'Support Beaker'}})
     await rootDrive.pda.writeFile(`/bookmarks/beaker-dev-docs-templates.goto`, '', {metadata: {href: 'https://beaker.dev/docs/templates/', title: 'Hyperdrive Templates'}})
     await rootDrive.pda.writeFile(`/bookmarks/twitter.goto`, '', {metadata: {href: 'https://twitter.com/', title: 'Twitter'}})
     await rootDrive.pda.writeFile(`/bookmarks/reddit.goto`, '', {metadata: {href: 'https://reddit.com/', title: 'Reddit'}})
     await rootDrive.pda.writeFile(`/bookmarks/youtube.goto`, '', {metadata: {href: 'https://youtube.com/', title: 'YouTube'}})
     await rootDrive.pda.mkdir('/beaker')
     await rootDrive.pda.writeFile(`/beaker/pins.json`, JSON.stringify([
-      'https://patreon.com/andrew_maf_and_paul',
+      'https://patreon.com/paul_maf_and_andrew',
       'https://beaker.dev/docs/templates/',
       'https://twitter.com/',
       'https://reddit.com/',
